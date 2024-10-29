4a45bb5c2c1dbfc6fa616d2cf4bb30c260094617
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-29 09:35:29 +0100

    refactor module enabled initialization

--- src/app/modules/local-admin/local-admin-it-system-usage/local-admin-it-system-usage.component.ts
+++ src/app/modules/local-admin/local-admin-it-system-usage/local-admin-it-system-usage.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { LocalAdminModuleSegmentOptions, LocalAdminModuleSegmentOptionType } from 'src/app/shared/constants/local-admin-module-segment-constants';
 import { UIModuleConfigKey } from 'src/app/shared/enums/ui-module-config-key';
-import { selectShowItSystemModule } from 'src/app/store/organization/selectors';
 
 @Component({
   selector: 'app-local-admin-it-system-usage',
@@ -13,9 +11,6 @@ export class LocalAdminItSystemUsageComponent {
   public readonly LocalAdminModuleSegmentOptionType = LocalAdminModuleSegmentOptionType;
   public readonly segmentOptions = LocalAdminModuleSegmentOptions;
   public readonly itSystemUsageModuleKey = UIModuleConfigKey.ItSystemUsage;
-  public readonly showItSystemModule$ = this.store.select(selectShowItSystemModule);
 
   public selectedSegment = LocalAdminModuleSegmentOptionType.UiCustomization;
-  constructor(private readonly store: Store) {
-  }
 }
--- src/app/modules/local-admin/ui-config/ui-config.component.ts
+++ src/app/modules/local-admin/ui-config/ui-config.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { UIModuleConfigKey } from 'src/app/shared/enums/ui-module-config-key';
 import { UIModuleConfig } from 'src/app/shared/models/ui-config/ui-module-config.model';
+import { selectModuleVisibility } from 'src/app/store/organization/selectors';
 import { selectModuleConfig } from 'src/app/store/organization/ui-module-customization/selectors';
 
 @Component({
@@ -15,12 +16,13 @@ export class UiConfigComponent implements OnInit{
   constructor(private store: Store) {}
 
   @Input() moduleKey!: UIModuleConfigKey;
-  @Input() moduleEnabled$!: Observable<boolean | undefined>;
 
   public uiConfig$!: Observable<UIModuleConfig | undefined>;
+  public moduleEnabled$!: Observable<boolean | undefined>;
 
   ngOnInit(): void {
     this.uiConfig$ = this.store.select(selectModuleConfig(this.moduleKey));
+    this.moduleEnabled$ = this.store.select(selectModuleVisibility(this.moduleKey));
   }
 
 }
