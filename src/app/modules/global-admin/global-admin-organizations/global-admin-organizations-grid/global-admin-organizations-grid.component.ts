ef2d398807f23fb3858ad51046b1e04547f324cb
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-04 15:24:04 +0100

    refresh grid when creating/updating organizations

--- src/app/modules/global-admin/global-admin-organizations/global-admin-organizations-grid/global-admin-organizations-grid.component.ts
+++ src/app/modules/global-admin/global-admin-organizations/global-admin-organizations-grid/global-admin-organizations-grid.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { first, of } from 'rxjs';
+import { combineLatestWith, first, of } from 'rxjs';
 import { BaseOverviewComponent } from 'src/app/shared/base/base-overview.component';
 import { ORGANIZATION_SECTION_NAME } from 'src/app/shared/constants/persistent-state-constants';
 import { GridActionColumn } from 'src/app/shared/models/grid-action-column.model';
@@ -17,6 +17,7 @@ import {
 } from 'src/app/store/organization/selectors';
 import { CreateOrganizationDialogComponent } from '../create-organization-dialog/create-organization-dialog.component';
 import { EditOrganizationDialogComponent } from '../edit-organization-dialog/edit-organization-dialog.component';
+import { Actions, ofType } from '@ngrx/effects';
 
 @Component({
   selector: 'app-global-admin-organizations-grid',
@@ -72,11 +73,22 @@ export class GlobalAdminOrganizationsGridComponent extends BaseOverviewComponent
 
   public readonly gridColumns$ = of(this.gridColumns);
 
-  constructor(store: Store, private dialog: MatDialog) {
+  constructor(store: Store, private dialog: MatDialog, private actions$: Actions) {
     super(store, 'global-admin-organization');
   }
   ngOnInit() {
     this.gridState$.pipe(first()).subscribe((gridState) => this.stateChange(gridState));
+
+    this.subscriptions.add(
+      this.actions$
+        .pipe(
+          ofType(OrganizationActions.createOrganizationSuccess, OrganizationActions.patchOrganizationSuccess),
+          combineLatestWith(this.gridState$)
+        )
+        .subscribe(([_, gridState]) => {
+          this.stateChange(gridState);
+        })
+    );
   }
 
   public stateChange(gridState: GridState) {
