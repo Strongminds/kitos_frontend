9052ac4151586becbf7f79cf14f08694c777816d
NikolajNielsen <nan@strongminds.dk>
2024-11-05 20:38:27 +0100

    Fix injection

--- src/app/shared/components/global-option-type-table/global-option-type-table.component.ts
+++ src/app/shared/components/global-option-type-table/global-option-type-table.component.ts
@@ -4,13 +4,14 @@ import { Actions, ofType } from '@ngrx/effects';
 import { filter } from 'rxjs';
 import { GlobalOptionTypeActions } from 'src/app/store/global-admin/actions';
 import { BaseComponent } from '../../base/base.component';
-import { GlobalOptionTypeTableComponentStore } from './global-option-type-table.component-store';
 import { GlobalAdminOptionType, GlobalAdminOptionTypeItem } from '../../models/options/global-admin-option-type.model';
+import { GlobalOptionTypeTableComponentStore } from './global-option-type-table.component-store';
 
 @Component({
   selector: 'app-global-option-type-table',
   templateUrl: './global-option-type-table.component.html',
   styleUrl: './global-option-type-table.component.scss',
+  providers: [GlobalOptionTypeTableComponentStore],
 })
 export class GlobalOptionTypeTableComponent extends BaseComponent implements OnInit {
   @Input() optionType!: GlobalAdminOptionType;
@@ -34,7 +35,10 @@ export class GlobalOptionTypeTableComponent extends BaseComponent implements OnI
     this.subscriptions.add(
       this.actions$
         .pipe(
-          ofType(GlobalOptionTypeActions.updateRegularOptionTypeSuccess),
+          ofType(
+            GlobalOptionTypeActions.updateRegularOptionTypeSuccess,
+            GlobalOptionTypeActions.createRegularOptionTypeSuccess
+          ),
           filter(({ optionType }) => optionType === this.optionType)
         )
         .subscribe(() => {
