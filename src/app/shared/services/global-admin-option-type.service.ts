c938c15fa0a02fd7b15529491ff76a22b93a2c30
NikolajNielsen <nan@strongminds.dk>
2024-11-06 07:40:21 +0100

    Effect can patch enabled state

--- src/app/shared/services/global-admin-option-type.service.ts
+++ src/app/shared/services/global-admin-option-type.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {
   APIGlobalRoleOptionResponseDTO,
+  APIGlobalRoleOptionUpdateRequestDTO,
   APIV2ItSystemGlobalBusinessTypesInternalINTERNALService,
 } from 'src/app/api/v2';
 import { GlobalAdminOptionType } from '../models/options/global-admin-option-type.model';
@@ -19,12 +20,33 @@ export class GlobalAdminOptionTypeService {
     return this.resolveGetGlobalOptionsEndpoint(optionType)();
   }
 
+  public patchGlobalOption(
+    optionType: GlobalAdminOptionType,
+    optionUuid: string,
+    request: APIGlobalRoleOptionUpdateRequestDTO
+  ) {
+    return this.resolvePatchGlobalOptionEndpoint(optionType)(optionUuid, request);
+  }
+
   private resolveGetGlobalOptionsEndpoint(
     optionType: GlobalAdminOptionType
   ): () => Observable<Array<APIGlobalRoleOptionResponseDTO>> {
     switch (optionType) {
       case 'it-system_business-type':
         return () => this.businessTypeService.getManyItSystemGlobalBusinessTypesInternalV2GetBusinessTypes();
+      default:
+        throw new Error(`Get operation is not supported for ${optionType}`);
+    }
+  }
+
+  private resolvePatchGlobalOptionEndpoint(optionType: GlobalAdminOptionType) {
+    switch (optionType) {
+      case 'it-system_business-type':
+        return (optionUuid: string, request: APIGlobalRoleOptionUpdateRequestDTO) =>
+          this.businessTypeService.patchSingleItSystemGlobalBusinessTypesInternalV2PatchGlobalBusinessType({
+            optionUuid,
+            dto: request,
+          });
       default:
         throw new Error(`Get operation is not supported for ${optionType}`);
     }
--- src/app/store/global-admin/effects.ts
+++ src/app/store/global-admin/effects.ts
@@ -1,13 +1,29 @@
-import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
-import { Store } from "@ngrx/store";
-import { GlobalAdminOptionTypeService } from "src/app/shared/services/global-admin-option-type.service";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, concatMap, groupBy, map, mergeMap, of } from 'rxjs';
+import { GlobalAdminOptionTypeService } from 'src/app/shared/services/global-admin-option-type.service';
+import { GlobalOptionTypeActions } from './actions';
 
 @Injectable()
 export class GlobalAdminOptionTypeEffects {
-  constructor( private store: Store,
-    private actions$: Actions,
-    private globalOptionTypeService: GlobalAdminOptionTypeService) {}
+  constructor(private actions$: Actions, private globalOptionTypeService: GlobalAdminOptionTypeService) {}
 
-    
+  patchOptionType$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(GlobalOptionTypeActions.updateRegularOptionType),
+      groupBy((action) => action.optionUuid),
+      mergeMap((group$) =>
+        group$.pipe(
+          concatMap((action) => {
+            return this.globalOptionTypeService
+              .patchGlobalOption(action.optionType, action.optionUuid, action.request)
+              .pipe(
+                map(() => GlobalOptionTypeActions.updateRegularOptionTypeSuccess(action.optionType)),
+                catchError(() => of(GlobalOptionTypeActions.updateRegularOptionTypeError()))
+              );
+          })
+        )
+      )
+    );
+  });
 }
