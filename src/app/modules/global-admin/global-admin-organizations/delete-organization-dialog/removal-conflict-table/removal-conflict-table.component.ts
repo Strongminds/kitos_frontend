70430509a9c165a636f4f7e18a12aee163dd7452
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-07 13:50:07 +0100

    refactor conflict table to take a type, and handle the strings and titles in there instead

--- src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/removal-conflict-table/removal-conflict-table.component.ts
+++ src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/removal-conflict-table/removal-conflict-table.component.ts
@@ -8,12 +8,42 @@ import { Observable } from 'rxjs';
 })
 export class RemovalConflictTableComponent {
   @Input() public removalConflicts$!: Observable<RemovalConflict[] | undefined>;
-  @Input() public header!: RemovalConflictHeader;
-  @Input() public title!: string;
-  @Input() public tooltipText!: string;
+  @Input() public type!: RemovalConflictType;
 
-  public hasMainEntity(): boolean {
-    return this.header.mainEntityTitle !== undefined;
+  public getAccordionTitle(): string {
+    switch (this.type) {
+      case 'contracts':
+        return 'en titel';
+      default:
+        throw new Error(`Unknown removal conflict type: ${this.type}`);
+    }
+  }
+
+  public getEntityTitle(): string {
+    switch (this.type) {
+      case 'contracts':
+        return 'normal';
+      default:
+        throw new Error(`Unknown removal conflict type: ${this.type}`);
+    }
+  }
+
+  public getMainEntityTitle(): string | undefined {
+    switch (this.type) {
+      case 'contracts':
+        return undefined;
+      default:
+        throw new Error(`Unknown removal conflict type: ${this.type}`);
+    }
+  }
+
+  public getTooltipText(): string {
+    switch (this.type) {
+      case 'contracts':
+        return 'tooltip';
+      default:
+        throw new Error(`Unknown removal conflict type: ${this.type}`);
+    }
   }
 }
 
@@ -27,3 +57,5 @@ export interface RemovalConflict {
   entityName: string;
   organizationName: string;
 }
+
+export type RemovalConflictType = 'contracts' | 'something';
