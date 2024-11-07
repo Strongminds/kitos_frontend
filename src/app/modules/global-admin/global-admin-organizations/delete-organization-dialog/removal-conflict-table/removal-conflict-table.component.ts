1e4a7eac770f8db1ba7523a5a5e3c553268ce4fb
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-07 13:40:41 +0100

    refactor removal table to take observable instead, to reduce boilerplate

--- src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/removal-conflict-table/removal-conflict-table.component.ts
+++ src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/removal-conflict-table/removal-conflict-table.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-removal-conflict-table',
   templateUrl: './removal-conflict-table.component.html',
   styleUrl: './removal-conflict-table.component.scss',
 })
-export class RemovalConflictTableComponent implements OnInit {
-  @Input() public removalConflicts!: RemovalConflict[];
+export class RemovalConflictTableComponent {
+  @Input() public removalConflicts$!: Observable<RemovalConflict[] | undefined>;
   @Input() public header!: RemovalConflictHeader;
   @Input() public title!: string;
   @Input() public tooltipText!: string;
-
-  public ngOnInit(): void {
-    console.log(this.removalConflicts);
-  }
 
   public hasMainEntity(): boolean {
     return this.header.mainEntityTitle !== undefined;
