6ce23cba225c95006980f80082f721ea7a4aec16
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-07 11:09:11 +0100

    make checkbox for understanding consequences and make delete button disabled depending on properties

--- src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/delete-organization-dialog.component.ts
+++ src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/delete-organization-dialog.component.ts
@@ -1,22 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { map, Observable, of } from 'rxjs';
 import { Organization } from 'src/app/shared/models/organization/organization.model';
 
 @Component({
   selector: 'app-delete-organization-dialog',
   templateUrl: './delete-organization-dialog.component.html',
-  styleUrl: './delete-organization-dialog.component.scss'
+  styleUrl: './delete-organization-dialog.component.scss',
 })
 export class DeleteOrganizationDialogComponent {
   @Input() public organization!: Organization;
 
+  public hasAcceptedConsequences: boolean = false;
+
   constructor(private dialogRef: MatDialogRef<DeleteOrganizationDialogComponent>) {}
 
-  public onDelete(): void {
-    
-  }
+  public onDelete(): void {}
 
   public onCancel(): void {
     this.dialogRef.close();
+  }
+
+  public hasConflicts(): Observable<boolean | undefined> {
+    return of(true);
+  }
+
+  public getTitle(): string {
+    return $localize`Slet` + ' "' + this.organization.Name + '"';
+  }
+
+  public canSubmit(): Observable<boolean> {
+    return this.hasConflicts().pipe(map((hasConflicts) => {
+      return hasConflicts === false || this.hasAcceptedConsequences;
+    }));
   }
 }
