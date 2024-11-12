2000568dfb7adedb8c5dfe3179db6fe4102fe147
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-12 10:46:27 +0100

    add new component for making the table

---
+++ src/app/modules/global-admin/global-admin-global-admins/global-admins-table/global-admins-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-admins-table',
+  templateUrl: './global-admins-table.component.html',
+  styleUrl: './global-admins-table.component.scss'
+})
+export class GlobalAdminsTableComponent {
+
+}
