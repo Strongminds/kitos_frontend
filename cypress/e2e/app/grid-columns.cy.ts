974132d8521a9851c88448ce8872661b5c1d4cb2
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-12 10:23:01 +0100

    change grid column tests

--- cypress/e2e/app/grid-columns.cy.ts
+++ cypress/e2e/app/grid-columns.cy.ts
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 describe('grid columns', () => {
+
+  //eslint-disable-next-line
+  let cache: {columns: any[], hash: string};
+
   beforeEach(() => {
     cy.intercept('/odata/DataProcessingRegistrationReadModels*', {
       fixture: './dpr/data-processings-odata.json',
@@ -12,48 +16,27 @@ describe('grid columns', () => {
     cy.intercept('/api/v2/data-processing-registrations/permissions*', { fixture: 'shared/create-permissions.json' });
     cy.intercept('/api/v2/internal/organizations/*/grid/permissions', { statusCode: 404, body: {} });
     cy.intercept('/api/v2/internal/organizations/*/grid/*/*', { statusCode: 404, body: {} });
+
+    cy.clearAllLocalStorage();
+    cy.setup(true, 'data-processing');
+    cy.wait(2000);
+    cy.window().then((window) => {
+      const dprGridColumnsString = window.localStorage.getItem('data-processing-grid-columns') ?? '';
+      cache = JSON.parse(dprGridColumnsString);
+    });
+
   });
 
-  it('Doesnt invalidate columns when width, order and visibility has been changed', () => {
-    cy.fixture('./shared/columns-with-width-order-and-visibility-changed.json').then((data) => {
-      const json = JSON.stringify(data);
-      localStorage.setItem('data-processing-grid-columns', JSON.stringify(data));
-
-      cy.wrap(json).as('initialValue');
-    });
-
-    cy.setup(true, 'data-processing');
+  it('Doesnt invalidate columns when visibility has been changed', () => {
+    cy.contains('Tilpas kolonner').click();
+    cy.getByDataCy('checkbox-input').eq(2).click()
+    cy.contains('Gem').click();
+    // Assertions
 
     cy.window().then((window) => {
-      const newValue = window.localStorage.getItem('data-processing-grid-columns');
-      cy.wrap(newValue).as('newValue');
-    });
-
-    cy.get('@initialValue').then((initialValue) => {
-      cy.get('@newValue').then((newValue) => {
-        expect(newValue).to.equal(initialValue);
-      });
-    });
-  });
-
-  it('Invalidates columns when title has changed', () => {
-    cy.fixture('./shared/columns-with-different-title.json').then((data) => {
-      const json = JSON.stringify(data);
-      localStorage.setItem('data-processing-grid-columns', JSON.stringify(data));
-      cy.wrap(json).as('initialValue');
-    });
-
-    cy.setup(true, 'data-processing');
-
-    cy.window().then((window) => {
-      const newValue = window.localStorage.getItem('data-processing-grid-columns');
-      cy.wrap(newValue).as('newValue');
-    });
-
-    cy.get('@initialValue').then((initialValue) => {
-      cy.get('@newValue').then((newValue) => {
-        expect(newValue).to.not.equal(initialValue);
-      });
+      const dprGridColumnsString = window.localStorage.getItem('data-processing-grid-columns') ?? '';
+      const newCache = JSON.parse(dprGridColumnsString);
+      expect(newCache.hash).to.eq(cache.hash);
     });
   });
 });
