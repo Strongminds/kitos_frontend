b946566bd33f880fe1a062f0b55c2f6db7af6075
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-12 11:15:58 +0100

    more scaffolding for global admin table

--- src/app/modules/global-admin/global-admin-global-admins/global-admins-table/global-admins-table.component.ts
+++ src/app/modules/global-admin/global-admin-global-admins/global-admins-table/global-admins-table.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ConfirmActionCategory, ConfirmActionService } from 'src/app/shared/services/confirm-action.service';
 
 @Component({
   selector: 'app-global-admins-table',
   templateUrl: './global-admins-table.component.html',
-  styleUrl: './global-admins-table.component.scss'
+  styleUrl: './global-admins-table.component.scss',
 })
 export class GlobalAdminsTableComponent {
+  constructor(private confirmActionService: ConfirmActionService) {}
 
+  public readonly globalAdmins: object[] = [{}];
+
+  public removeGlobalAdmin(globalAdmin: object) {
+    console.log(globalAdmin);
+    this.confirmActionService.confirmAction({
+      category: ConfirmActionCategory.Warning,
+      message: $localize`Fjern ${globalAdmin} som global administrator`,
+      onConfirm: () => this.deleteGlobalAdmin(globalAdmin),
+    });
+  }
+
+  private deleteGlobalAdmin(globalAdmin: object) {}
 }
