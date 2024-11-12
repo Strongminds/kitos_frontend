a1dcf45e812d5018d4e3444f7baf787aa1f639ee
NikolajNielsen <nan@strongminds.dk>
2024-11-12 10:08:17 +0100

    Simple table with no-op buttons

--- src/app/modules/global-admin/global-admin-help-texts/global-admin-help-texts.component.ts
+++ src/app/modules/global-admin/global-admin-help-texts/global-admin-help-texts.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { APIHelpTextResponseDTO } from 'src/app/api/v2/model/helpTextResponseDTO';
+import { HelpTextActions } from 'src/app/store/global-admin/help-texts/actions';
+import { selectHelpTexts } from 'src/app/store/global-admin/help-texts/selectors';
 
 @Component({
   selector: 'app-global-admin-help-texts',
   templateUrl: './global-admin-help-texts.component.html',
-  styleUrl: './global-admin-help-texts.component.scss'
+  styleUrl: './global-admin-help-texts.component.scss',
 })
-export class GlobalAdminHelpTextsComponent {
+export class GlobalAdminHelpTextsComponent implements OnInit {
+  public readonly helpTexts$ = this.store.select(selectHelpTexts);
 
+  constructor(private readonly store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(HelpTextActions.getHelpTexts());
+  }
+
+  public onEdit(helpText: APIHelpTextResponseDTO) {}
+
+  public onDelete(helpText: APIHelpTextResponseDTO) {}
 }
