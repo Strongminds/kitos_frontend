18c32175d3f2cc7ce60881c9d3cb9f32621a43c8
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-12 13:42:57 +0100

    move org removal conflicts models and helpers to their own files

--- src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/delete-organization.component-store.ts
+++ src/app/modules/global-admin/global-admin-organizations/delete-organization-dialog/delete-organization.component-store.ts
@@ -2,15 +2,9 @@ import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { tapResponse } from '@ngrx/operators';
 import { map, mergeMap, Observable, tap } from 'rxjs';
-import {
-  APIInterfacesExposedOutsideTheOrganizationResponseDTO,
-  APIMultipleConflictsResponseDTO,
-  APIOrganizationRemovalConflictsResponseDTO,
-  APISimpleConflictResponseDTO,
-  APISystemWithUsageOutsideOrganizationConflictResponseDTO,
-  APIV2OrganizationsInternalINTERNALService,
-} from 'src/app/api/v2';
-import { RemovalConflict } from './removal-conflict-table/removal-conflict-table.component';
+import { APIV2OrganizationsInternalINTERNALService } from 'src/app/api/v2';
+import { mapConflictsDtoToOrganizationRemovalConflicts } from 'src/app/shared/helpers/removal-conflicts.helper';
+import { OrganizationRemovalConflicts } from 'src/app/shared/models/global-admin/organization-removal-conflicts.model';
 
 interface State {
   conflicts?: OrganizationRemovalConflicts;
@@ -20,7 +14,7 @@ interface State {
 @Injectable()
 export class DeleteOrganizationComponentStore extends ComponentStore<State> {
   constructor(private apiService: APIV2OrganizationsInternalINTERNALService) {
-    super({ conflicts: undefined, isLoading: false,  });
+    super({ conflicts: undefined, isLoading: false });
   }
 
   public readonly conflicts$ = this.select((state) => state.conflicts);
@@ -50,81 +44,4 @@ export class DeleteOrganizationComponentStore extends ComponentStore<State> {
       )
     )
   );
-}
-
-export interface OrganizationRemovalConflicts {
-  contractsInOtherOrganizationsWhereOrgIsSupplier: RemovalConflict[];
-  dprInOtherOrganizationsWhereOrgIsDataProcessor: RemovalConflict[];
-  dprInOtherOrganizationsWhereOrgIsSubDataProcessor: RemovalConflict[];
-  systemsExposingInterfacesDefinedInOtherOrganizations: RemovalConflict[];
-  systemsSetAsParentSystemToSystemsInOtherOrganizations: RemovalConflict[];
-  systemsInOtherOrganizationsWhereOrgIsRightsHolder: RemovalConflict[];
-  systemsWhereOrgIsArchiveSupplier: RemovalConflict[];
-  interfacesExposedOnSystemsOutsideTheOrganization: RemovalConflict[];
-  systemsWithUsagesOutsideTheOrganization: RemovalConflict[];
-}
-
-function mapConflictsDtoToOrganizationRemovalConflicts(
-  conflictsDto: APIOrganizationRemovalConflictsResponseDTO
-): OrganizationRemovalConflicts {
-  const conflicts: OrganizationRemovalConflicts = {
-    contractsInOtherOrganizationsWhereOrgIsSupplier:
-      conflictsDto.contractsInOtherOrganizationsWhereOrgIsSupplier?.map(mapSimpleConflictToTableItem) ?? [],
-    dprInOtherOrganizationsWhereOrgIsDataProcessor:
-      conflictsDto.dprInOtherOrganizationsWhereOrgIsDataProcessor?.map(mapSimpleConflictToTableItem) ?? [],
-    dprInOtherOrganizationsWhereOrgIsSubDataProcessor:
-      conflictsDto.dprInOtherOrganizationsWhereOrgIsSubDataProcessor?.map(mapSimpleConflictToTableItem) ?? [],
-    interfacesExposedOnSystemsOutsideTheOrganization:
-      conflictsDto.interfacesExposedOnSystemsOutsideTheOrganization?.map(
-        mapInterfacesExposedOutsideTheOrganizationToTableItem
-      ) ?? [],
-    systemsExposingInterfacesDefinedInOtherOrganizations:
-      conflictsDto.systemsExposingInterfacesDefinedInOtherOrganizations?.flatMap(mapMultipleConflictsToTableItem) ?? [],
-    systemsInOtherOrganizationsWhereOrgIsRightsHolder:
-      conflictsDto.systemsInOtherOrganizationsWhereOrgIsRightsHolder?.map(mapSimpleConflictToTableItem) ?? [],
-    systemsSetAsParentSystemToSystemsInOtherOrganizations:
-      conflictsDto.systemsSetAsParentSystemToSystemsInOtherOrganizations?.flatMap(mapMultipleConflictsToTableItem) ??
-      [],
-    systemsWhereOrgIsArchiveSupplier:
-      conflictsDto.systemsWhereOrgIsArchiveSupplier?.map(mapSimpleConflictToTableItem) ?? [],
-    systemsWithUsagesOutsideTheOrganization:
-      conflictsDto.systemsWithUsagesOutsideTheOrganization?.flatMap(
-        mapSystemUsageOutsideOrganizationConflictToTableItem
-      ) ?? [],
-  };
-  return conflicts;
-}
-
-function mapSimpleConflictToTableItem(conflict: APISimpleConflictResponseDTO): RemovalConflict {
-  return {
-    mainEntityName: undefined,
-    entityName: conflict.entityName ?? '',
-    organizationName: conflict.organizationName ?? '',
-  };
-}
-
-function mapMultipleConflictsToTableItem(multiConflict: APIMultipleConflictsResponseDTO): RemovalConflict[] {
-  return (multiConflict.conflicts ?? [])
-    .map(mapSimpleConflictToTableItem)
-    .map((conflict) => ({ ...conflict, mainEntityName: multiConflict.mainEntityName }));
-}
-
-function mapSystemUsageOutsideOrganizationConflictToTableItem(
-  conflict: APISystemWithUsageOutsideOrganizationConflictResponseDTO
-): RemovalConflict[] {
-  return (conflict.organizationNames ?? []).map((organizationName) => ({
-    mainEntityName: undefined,
-    entityName: conflict.systemName ?? '',
-    organizationName: organizationName ?? '',
-  }));
-}
-
-function mapInterfacesExposedOutsideTheOrganizationToTableItem(
-  conflict: APIInterfacesExposedOutsideTheOrganizationResponseDTO
-): RemovalConflict {
-  return {
-    mainEntityName: conflict.exposedInterfaceName,
-    entityName: conflict.exposingSystemName ?? '',
-    organizationName: conflict.organizationName ?? '',
-  };
 }
