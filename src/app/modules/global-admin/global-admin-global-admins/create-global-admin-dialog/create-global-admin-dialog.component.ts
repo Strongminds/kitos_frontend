409f94c7ae66ff59f08f30bccea40a5045728b90
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-13 15:51:05 +0100

    filter out global admins from dropdown in create global admin dialog

--- src/app/modules/global-admin/global-admin-global-admins/create-global-admin-dialog/create-global-admin-dialog.component.ts
+++ src/app/modules/global-admin/global-admin-global-admins/create-global-admin-dialog/create-global-admin-dialog.component.ts
@@ -1,24 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { selectAll } from 'src/app/store/organization/organization-user/selectors';
+import { CreateGlobalAdminComponentStore } from './create-global-admin.component-store';
+import { APIUserReferenceResponseDTO } from 'src/app/api/v2';
 import { GlobalAdminActions } from 'src/app/store/global-admin/actions';
+import { BaseComponent } from 'src/app/shared/base/base.component';
+import { Actions, ofType } from '@ngrx/effects';
 
 @Component({
   selector: 'app-create-global-admin-dialog',
   templateUrl: './create-global-admin-dialog.component.html',
   styleUrl: './create-global-admin-dialog.component.scss',
+  providers: [CreateGlobalAdminComponentStore],
 })
-export class CreateGlobalAdminDialogComponent implements OnInit {
+export class CreateGlobalAdminDialogComponent extends BaseComponent implements OnInit {
+  public formGroup = new FormGroup({
+    user: new FormControl<APIUserReferenceResponseDTO | undefined>(undefined, Validators.required),
+  });
 
-  constructor(private dialogRef: MatDialogRef<CreateGlobalAdminDialogComponent>, private store: Store) {}
+  constructor(
+    private dialogRef: MatDialogRef<CreateGlobalAdminDialogComponent>,
+    private store: Store,
+    private componentStore: CreateGlobalAdminComponentStore,
+    private actions$: Actions
+  ) {
+    super();
+  }
+
+  public readonly users$ = this.componentStore.users$;
+  public readonly loading$ = this.componentStore.loading$;
 
   public ngOnInit(): void {
-    this.store.dispatch(GlobalAdminActions.getGlobalAdmins());
+    this.subscriptions.add(
+      this.actions$.pipe(ofType(GlobalAdminActions.addGlobalAdminSuccess)).subscribe(() => this.close())
+    );
   }
 
   public close(): void {
     this.dialogRef.close();
   }
 
-  public addGlobalAdmin(): void {}
+  public addGlobalAdmin(): void {
+    const selectedUser = this.formGroup.value.user ?? undefined;
+    if (!selectedUser) throw new Error('No user selected');
+    this.store.dispatch(GlobalAdminActions.addGlobalAdmin(selectedUser.uuid));
+  }
+
+  public searchUsers(search: string): void {
+    this.componentStore.searchUsers(search);
+  }
 }
