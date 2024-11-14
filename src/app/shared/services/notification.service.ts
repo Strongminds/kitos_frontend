31f1f323fbab0f7e2c0ff873a9e32808c0d903e7
NikolajNielsen <nan@strongminds.dk>
2024-11-14 08:19:02 +0100

    Notifications

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -66,6 +66,10 @@ export class NotificationService implements OnDestroy {
   private subscriptToHelpTextNotifications(){
     this.subscribeAsDefault(HelpTextActions.createHelpTextSuccess, $localize`Hjælpeteksten blev oprettet`);
     this.subscribeAsError(HelpTextActions.createHelpTextError, $localize`Kunne ikke oprette hjælpeteksten`);
+    this.subscribeAsDefault(HelpTextActions.deleteHelpTextSuccess, $localize`Hjælpeteksten blev slettet`);
+    this.subscribeAsError(HelpTextActions.deleteHelpTextError, $localize`Kunne ikke slette hjælpeteksten`);
+    this.subscribeAsDefault(HelpTextActions.updateHelpTextSuccess, $localize`Hjælpeteksten blev opdateret`);
+    this.subscribeAsError(HelpTextActions.updateHelpTextError, $localize`Kunne ikke opdatere hjælpeteksten`);
   }
 
   private subscribeToLocalAdminNotifications() {
