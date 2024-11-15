a7fc83a55289316169e180451bb7d3b28a2d103f
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-15 09:02:24 +0100

    setup for organization dropdown component + i18n

---
+++ src/app/shared/components/organization-dropdown/organization-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './organization-dropdown.component.html',
+  styleUrl: './organization-dropdown.component.scss'
+})
+export class OrganizationDropdownComponent {
+
+}
