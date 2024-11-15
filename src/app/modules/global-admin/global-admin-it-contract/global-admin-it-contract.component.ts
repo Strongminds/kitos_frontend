6518f61f3e3f9dea33e2dc486cf2303547b600b8
NikolajNielsen <nan@strongminds.dk>
2024-11-15 07:45:10 +0100

    Module tab base

--- src/app/modules/global-admin/global-admin-it-contract/global-admin-it-contract.component.ts
+++ src/app/modules/global-admin/global-admin-it-contract/global-admin-it-contract.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { GlobalAdminModuleSegmentOptions, GlobalAdminModuleSegmentOptionType } from 'src/app/shared/constants/global-admin-module-segment-constants';
+import { BaseGlobalAdminModuleTabComponent } from '../base-global-admin-module-tab';
 
 @Component({
   selector: 'app-global-admin-it-contract',
   templateUrl: './global-admin-it-contract.component.html',
   styleUrl: './global-admin-it-contract.component.scss'
 })
-export class GlobalAdminItContractComponent {
-  public readonly GlobalAdminModuleSegmentOptionType = GlobalAdminModuleSegmentOptionType;
-  public readonly segmentOptions = GlobalAdminModuleSegmentOptions;
-
-  public selectedSegment = GlobalAdminModuleSegmentOptionType.RegularOptionTypes;
+export class GlobalAdminItContractComponent extends BaseGlobalAdminModuleTabComponent {
 }
