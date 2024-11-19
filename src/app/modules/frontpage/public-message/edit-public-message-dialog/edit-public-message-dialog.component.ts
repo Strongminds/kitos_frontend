c5e6c96ace0e0242964a54104f010ed170849d04
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-19 14:17:37 +0100

    now closes dialog on success

--- src/app/modules/frontpage/public-message/edit-public-message-dialog/edit-public-message-dialog.component.ts
+++ src/app/modules/frontpage/public-message/edit-public-message-dialog/edit-public-message-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { APIPublicMessagesRequestDTO } from 'src/app/api/v2';
+import { BaseComponent } from 'src/app/shared/base/base.component';
 import { PublicMessageType } from 'src/app/shared/models/public-messages.model';
 import { GlobalAdminPublicMessageActions } from 'src/app/store/global-admin/public-messages/actions';
 
@@ -11,7 +13,7 @@ import { GlobalAdminPublicMessageActions } from 'src/app/store/global-admin/publ
   templateUrl: './edit-public-message-dialog.component.html',
   styleUrl: './edit-public-message-dialog.component.scss',
 })
-export class EditPublicMessageDialogComponent implements OnInit {
+export class EditPublicMessageDialogComponent extends BaseComponent implements OnInit {
   @Input() public message: string | undefined;
   @Input() public type!: PublicMessageType;
 
@@ -19,12 +21,24 @@ export class EditPublicMessageDialogComponent implements OnInit {
     message: new FormControl<string | undefined>(undefined),
   });
 
-  constructor(private store: Store, private dialogRef: MatDialogRef<EditPublicMessageDialogComponent>) {}
+  constructor(
+    private store: Store,
+    private actions$: Actions,
+    private dialogRef: MatDialogRef<EditPublicMessageDialogComponent>
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.formGroup.patchValue({
       message: this.message,
     });
+
+    this.subscriptions.add(
+      this.actions$.pipe(ofType(GlobalAdminPublicMessageActions.editPublicMessagesSuccess)).subscribe(() => {
+        this.close();
+      })
+    );
   }
 
   public close(): void {
