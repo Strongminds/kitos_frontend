a240f716880057aaf1d19d41a7348e430e862335
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-21 08:50:03 +0100

    remove unsued import

--- src/app/shared/components/grid/grid.component.ts
+++ src/app/shared/components/grid/grid.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
