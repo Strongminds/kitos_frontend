a119dfa88206f4ea495106914227326499e60f83
NikolajNielsen <nan@strongminds.dk>
2024-11-25 08:54:52 +0100

    Navigate on login based on user start preferences

--- src/app/modules/layout/nav-bar/nav-bar.component.ts
+++ src/app/modules/layout/nav-bar/nav-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NavigationEnd, Router } from '@angular/router';
+import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { combineLatest, filter, tap } from 'rxjs';
+import { APIUserResponseDTO } from 'src/app/api/v2';
 import { BaseComponent } from 'src/app/shared/base/base.component';
+import { StartPreferenceChoice } from 'src/app/shared/models/organization/organization-user/start-preference.model';
 import { OrganizationActions } from 'src/app/store/organization/actions';
 import { selectUIRootConfig } from 'src/app/store/organization/selectors';
 import { UserActions } from 'src/app/store/user-store/actions';
@@ -24,12 +27,18 @@ export class NavBarComponent extends BaseComponent implements OnInit {
   public readonly hasMultipleOrganizations$ = this.store.select(selectHasMultipleOrganizations);
   public readonly uiRootConfig$ = this.store.select(selectUIRootConfig);
 
-  constructor(private store: Store, private dialog: MatDialog, private router: Router) {
+  constructor(private store: Store, private dialog: MatDialog, private router: Router, private actions$: Actions) {
     super();
   }
 
   ngOnInit(): void {
     this.store.dispatch(OrganizationActions.getUIRootConfig());
+    this.subscriptions.add(
+      this.actions$.pipe(ofType(UserActions.loginSuccess)).subscribe(({ user }) => {
+        if (user?.defaultStartPage) this.navigateToUserDefaultStartPage(user.defaultStartPage);
+      })
+    );
+
     this.subscriptions.add(
       combineLatest([this.user$, this.router.events])
         .pipe(
@@ -38,6 +47,30 @@ export class NavBarComponent extends BaseComponent implements OnInit {
         )
         .subscribe()
     );
+  }
+
+  private navigateToUserDefaultStartPage(defaultStartPage: StartPreferenceChoice) {
+    const path = this.getDefaultStartPagePath(defaultStartPage);
+    this.router.navigate([path]);
+  }
+
+  private getDefaultStartPagePath(defaultStartPage: StartPreferenceChoice): string {
+    const startPageValue = defaultStartPage.value;
+    switch (startPageValue) {
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.StartSite:
+        return '';
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.Organization:
+        return `${AppPath.organization}/${AppPath.structure}`;
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.ItSystemCatalog:
+        return `${AppPath.itSystems}/${AppPath.itSystemCatalog}`;
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.ItSystemUsage:
+        return `${AppPath.itSystems}/${AppPath.itSystemUsages}`;
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.ItContract:
+        return AppPath.itContracts;
+      case APIUserResponseDTO.DefaultUserStartPreferenceEnum.DataProcessing:
+        return AppPath.dataProcessing;
+      default: throw new Error(`Unknown start page: ${startPageValue}`);
+    }
   }
 
   public showOrganizationDialog() {
