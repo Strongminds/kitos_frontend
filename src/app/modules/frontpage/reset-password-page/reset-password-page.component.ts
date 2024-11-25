cf2559630c43c1ef2a0535746e13d58e6bd96444
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-25 15:13:31 +0100

    finish styling for reset password page

--- src/app/modules/frontpage/reset-password-page/reset-password-page.component.ts
+++ src/app/modules/frontpage/reset-password-page/reset-password-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password-page',
   templateUrl: './reset-password-page.component.html',
-  styleUrl: './reset-password-page.component.scss'
+  styleUrl: './reset-password-page.component.scss',
 })
 export class ResetPasswordPageComponent {
-
+  public readonly formGroup = new FormGroup({
+    email: new FormControl<string | undefined>(undefined, [Validators.email]),
+  });
 }
