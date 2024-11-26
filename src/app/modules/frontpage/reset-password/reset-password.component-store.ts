2e4dc690176dfb26787c49e8597d6ca5f8648384
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-26 16:17:40 +0100

    introduce variable for reset password success

--- src/app/modules/frontpage/reset-password/reset-password.component-store.ts
+++ src/app/modules/frontpage/reset-password/reset-password.component-store.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { Actions, ofType } from '@ngrx/effects';
 import { tapResponse } from '@ngrx/operators';
 import { finalize, mergeMap, Observable, tap } from 'rxjs';
 import { APIPasswordResetResponseDTO, APIV2PasswordResetInternalINTERNALService } from 'src/app/api/v2';
+import { UserActions } from 'src/app/store/user-store/actions';
 
 interface State {
   email: string | undefined;
   loading: boolean;
+  resetPasswordSuccess: boolean;
 }
 
 @Injectable()
 export class ResetPasswordComponentStore extends ComponentStore<State> {
-  constructor(private resetPasswordService: APIV2PasswordResetInternalINTERNALService) {
-    super({ email: undefined, loading: true });
+  constructor(private resetPasswordService: APIV2PasswordResetInternalINTERNALService, private actions$: Actions) {
+    super({ email: undefined, loading: true, resetPasswordSuccess: false });
+
+    this.actions$.pipe(ofType(UserActions.resetPasswordSuccess)).subscribe(() => {
+      this.setSuccess(true);
+    });
   }
 
   public readonly email$ = this.select((state) => state.email);
   public readonly loading$ = this.select((state) => state.loading);
+  public readonly resetPasswordSuccess$ = this.select((state) => state.resetPasswordSuccess);
 
   public getPasswordResetRequest = this.effect((requestId$: Observable<string>) =>
     requestId$.pipe(
@@ -32,6 +40,10 @@ export class ResetPasswordComponentStore extends ComponentStore<State> {
         );
       })
     )
+  );
+
+  public readonly setSuccess = this.updater(
+    (state, resetPasswordSuccess: boolean): State => ({ ...state, resetPasswordSuccess })
   );
 
   public readonly setEmail = this.updater((state, email: string | undefined): State => ({ ...state, email }));
