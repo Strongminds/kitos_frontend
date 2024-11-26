f0d1fc7ac39f22f91f4322c8c0cf01081f04ca4a
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-26 15:23:39 +0100

    implement page for case where the requestId is invalid

--- src/app/modules/frontpage/reset-password/reset-password.component-store.ts
+++ src/app/modules/frontpage/reset-password/reset-password.component-store.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { tapResponse } from '@ngrx/operators';
-import { mergeMap, Observable, tap } from 'rxjs';
+import { finalize, mergeMap, Observable, tap } from 'rxjs';
 import { APIPasswordResetResponseDTO, APIV2PasswordResetInternalINTERNALService } from 'src/app/api/v2';
 
 interface State {
@@ -15,16 +15,20 @@ export class ResetPasswordComponentStore extends ComponentStore<State> {
     super({ email: undefined, loading: true });
   }
 
+  public readonly email$ = this.select((state) => state.email);
+  public readonly loading$ = this.select((state) => state.loading);
+
   public getPasswordResetRequest = this.effect((requestId$: Observable<string>) =>
     requestId$.pipe(
       tap(() => this.setLoading(true)),
       mergeMap((requestId) => {
         return this.resetPasswordService.getSinglePasswordResetInternalV2GetPasswordReset({ requestId }).pipe(
+          tap((response) => console.log(response)),
           tapResponse(
             (response: APIPasswordResetResponseDTO) => this.setEmail(response.email),
-            (error) => console.error(error),
-            () => this.setLoading(false)
-          )
+            (_) => this.setEmail(undefined)
+          ),
+          finalize(() => this.setLoading(false))
         );
       })
     )
--- src/app/modules/frontpage/reset-password/reset-password.component.ts
+++ src/app/modules/frontpage/reset-password/reset-password.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
 import { first, map, Observable } from 'rxjs';
-import { UserActions } from 'src/app/store/user-store/actions';
+import { ResetPasswordComponentStore } from './reset-password.component-store';
+import { AppPath } from 'src/app/shared/enums/app-path';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss',
+  providers: [ResetPasswordComponentStore],
 })
 export class ResetPasswordComponent implements OnInit {
-  public readonly requestId$: Observable<string> = this.route.params.pipe(
-    map((params) => params['id'])
-  );
+  public readonly requestId$: Observable<string> = this.route.params.pipe(map((params) => params['id']));
 
-  constructor(private route: ActivatedRoute, private store: Store) {}
+  public readonly email$: Observable<string | undefined> = this.componentStore.email$;
+  public readonly loading$: Observable<boolean> = this.componentStore.loading$;
+
+  public readonly routerLink = AppPath.passwordReset;
+  constructor(
+    private route: ActivatedRoute,
+    private componentStore: ResetPasswordComponentStore
+  ) {}
 
   public ngOnInit(): void {
-    this.requestId$.pipe(first()).subscribe((requestId) => {
-      this.store.dispatch(UserActions.getResetPasswordRequest(requestId));
-    });
+    this.requestId$.pipe(first()).subscribe((requestId) => this.componentStore.getPasswordResetRequest(requestId));
   }
 }
