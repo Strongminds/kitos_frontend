531ccef971029e9a3e1200b4bb07f1b067cb518f
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-26 16:29:14 +0100

    update api

--- src/app/api/v2/api/v2PasswordResetInternalINTERNAL.service.ts
+++ src/app/api/v2/api/v2PasswordResetInternalINTERNAL.service.ts
@@ -19,9 +19,11 @@ import { CustomHttpParameterCodec }                          from '../encoder';
 import { Observable }                                        from 'rxjs';
 
 // @ts-ignore
-import { APIPasswordResetRequestV2DTO } from '../model/aPIPasswordResetRequestV2DTO';
-// @ts-ignore
 import { APIPasswordResetResponseDTO } from '../model/aPIPasswordResetResponseDTO';
+// @ts-ignore
+import { APIRequestPasswordResetRequestDTO } from '../model/aPIRequestPasswordResetRequestDTO';
+// @ts-ignore
+import { APIResetPasswordRequestDTO } from '../model/aPIResetPasswordRequestDTO';
 
 // @ts-ignore
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
@@ -32,8 +34,13 @@ export interface GetSinglePasswordResetInternalV2GetPasswordResetRequestParams {
     requestId: string;
 }
 
+export interface PostSinglePasswordResetInternalV2PostPasswordResetRequestParams {
+    requestId: string;
+    request: APIResetPasswordRequestDTO;
+}
+
 export interface PostSinglePasswordResetInternalV2RequestPasswordResetRequestParams {
-    request: APIPasswordResetRequestV2DTO;
+    request: APIRequestPasswordResetRequestDTO;
 }
 
 
@@ -150,6 +157,78 @@ export class APIV2PasswordResetInternalINTERNALService {
         return this.httpClient.request<APIPasswordResetResponseDTO>('get', `${this.configuration.basePath}${localVarPath}`,
             {
                 context: localVarHttpContext,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * @param requestParameters
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public postSinglePasswordResetInternalV2PostPasswordReset(requestParameters: PostSinglePasswordResetInternalV2PostPasswordResetRequestParams, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<object>;
+    public postSinglePasswordResetInternalV2PostPasswordReset(requestParameters: PostSinglePasswordResetInternalV2PostPasswordResetRequestParams, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<object>>;
+    public postSinglePasswordResetInternalV2PostPasswordReset(requestParameters: PostSinglePasswordResetInternalV2PostPasswordResetRequestParams, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<object>>;
+    public postSinglePasswordResetInternalV2PostPasswordReset(requestParameters: PostSinglePasswordResetInternalV2PostPasswordResetRequestParams, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
+        const requestId = requestParameters.requestId;
+        if (requestId === null || requestId === undefined) {
+            throw new Error('Required parameter requestId was null or undefined when calling postSinglePasswordResetInternalV2PostPasswordReset.');
+        }
+        const request = requestParameters.request;
+        if (request === null || request === undefined) {
+            throw new Error('Required parameter request was null or undefined when calling postSinglePasswordResetInternalV2PostPasswordReset.');
+        }
+
+        let localVarHeaders = this.defaultHeaders;
+
+        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (localVarHttpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'application/json'
+            ];
+            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        let localVarHttpContext: HttpContext | undefined = options && options.context;
+        if (localVarHttpContext === undefined) {
+            localVarHttpContext = new HttpContext();
+        }
+
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Content-Type', httpContentTypeSelected);
+        }
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/api/v2/internal/users/password-reset/${this.configuration.encodeParam({name: "requestId", value: requestId, in: "path", style: "simple", explode: false, dataType: "string", dataFormat: undefined})}`;
+        return this.httpClient.request<object>('post', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                body: request,
                 responseType: <any>responseType_,
                 withCredentials: this.configuration.withCredentials,
                 headers: localVarHeaders,
