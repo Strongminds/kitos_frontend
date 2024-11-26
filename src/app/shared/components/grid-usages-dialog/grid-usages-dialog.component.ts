b142d24e40f2dbae367d00d7bbe4fd612aece5c8
NikolajNielsen <nan@strongminds.dk>
2024-11-26 14:20:56 +0100

    First shot at nested dialog

--- src/app/shared/components/grid-usages-dialog/grid-usages-dialog.component.ts
+++ src/app/shared/components/grid-usages-dialog/grid-usages-dialog.component.ts
@@ -1,5 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+import { selectOrganizationName } from 'src/app/store/user-store/selectors';
+import { filterNullish } from '../../pipes/filter-nullish';
+import { ConnectedDropdownDialogComponent } from '../dialogs/connected-dropdown-dialog/connected-dropdown-dialog.component';
 
 @Component({
   selector: 'app-usages',
@@ -7,5 +12,24 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./grid-usages-dialog.component.scss'],
 })
 export class GridUsagesDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { usages: string[]; title: string }) {}
+  public readonly organizationName$ = this.store.select(selectOrganizationName).pipe(filterNullish());
+  //todo make store setup for the migration endpoints
+  // public readonly unusedItSystems$ =
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { usages: string[]; title: string },
+    private readonly dialog: MatDialog,
+    private readonly store: Store
+  ) {}
+
+  public clickMigrateUsage() {
+    this.organizationName$.pipe(
+      map((organizationName) => {
+        const dialogRef = this.dialog.open(ConnectedDropdownDialogComponent);
+        const componentInstance = dialogRef.componentInstance;
+        componentInstance.title = $localize`Flytning af systemanvendelse for ${organizationName}`;
+        componentInstance.dropdownText = $localize`Vælg IT system`;
+      })
+    );
+  }
 }
