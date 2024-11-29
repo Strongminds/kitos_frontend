cbf13d53ac8d650b3cd1750f354b6bbddf5dff34
NikolajNielsen <nan@strongminds.dk>
2024-11-29 09:58:20 +0100

    Cypress test, missing assert

--- cypress/e2e/it-system-catalog/overview/it-system-catalog.overview.cy.ts
+++ cypress/e2e/it-system-catalog/overview/it-system-catalog.overview.cy.ts
@@ -10,6 +10,36 @@ describe('it-system-catalog', () => {
     cy.setup(true, 'it-systems/it-system-catalog');
   });
 
+  it('can migrate usage if global admin', () => {
+    const connectedDropdown = 'connected-dropdown-selector';
+    cy.intercept('/api/v2/internal/it-system-usages/migration/permissions', {
+      fixture: './it-system-catalog/migration-permissions.json',
+    });
+    cy.intercept('api/v2/internal/it-system-usages/migration/unused-it-systems?organizationUuid=**', {
+      fixture: './it-system-catalog/it-systems-v2.json',
+    });
+    cy.intercept('/api/v2/internal/it-system-usages/search*', {
+      fixture: './it-system-usage/it-system-usages-v2.json',
+    });
+    cy.intercept('api/v2/internal/it-system-usages/*/migration?*', {
+      fixture: './it-system-catalog/migration.json',
+    }).as('getMigration');
+    cy.intercept('POST', 'api/v2/internal/it-system-usages/*/migration?toSystemUuid=*').as('executeMigration');
+
+    cy.getByDataCy('grid-usages-link').first().click();
+    cy.getByDataCy('migrate-button').first().click();
+    cy.getByDataCy(connectedDropdown).type('System 2');
+    cy.dropdownByCy(connectedDropdown, 'System 2', true);
+    cy.getByDataCy('confirm-button').click();
+
+
+    cy.getByDataCy('execute-migration-button').click();
+    cy.wait('@executeMigration').then((interception) => {
+      console.log(interception.request.body)
+      expect(interception.request.body.toSystemUuid).to.equal('8d843a6f-2424-43d6-90b8-916a72e615ce');
+    });
+  });
+
   it('can show IT system usage grid', () => {
     cy.get('h3').should('have.text', 'IT Systemkatalog');
 
