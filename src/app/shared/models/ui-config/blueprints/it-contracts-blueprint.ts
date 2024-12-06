d769b24b38dc28f96723606573e412047eea1c2e
JacobBorchMadsen <jbma@strongminds.dk>
2024-12-06 09:18:11 +0100

    update blueprint

--- src/app/shared/models/ui-config/blueprints/it-contracts-blueprint.ts
+++ src/app/shared/models/ui-config/blueprints/it-contracts-blueprint.ts
@@ -1,5 +1,6 @@
-import { UIModuleConfigKey } from "src/app/shared/enums/ui-module-config-key";
-import { uiConfigHelpTexts } from "../ui-config-helptexts";
+import { UIModuleConfigKey } from 'src/app/shared/enums/ui-module-config-key';
+import { uiConfigHelpTexts } from '../ui-config-helptexts';
+import { last } from 'lodash';
 
 export const ItContractsUiBluePrint = {
   module: UIModuleConfigKey.ItContract,
@@ -12,55 +13,72 @@ export const ItContractsUiBluePrint = {
       isObligatory: true,
       helpText: uiConfigHelpTexts.cannotChangeTab,
       children: {
+        contractName: {
+          text: $localize`Kontraktnavn`,
+          isObligatory: true,
+        },
         contractId: {
-          text: $localize`KontraktID`
+          text: $localize`KontraktID`,
         },
         contractType: {
-          text: $localize`Kontrakttype`
+          text: $localize`Kontrakttype`,
         },
         template: {
-          text: $localize`Kontraktskabelon`
+          text: $localize`Kontraktskabelon`,
         },
         criticality: {
-          text: $localize`Kritikalitet`
+          text: $localize`Kritikalitet`,
         },
         purchaseForm: {
-          text: $localize`Indkøbsform`
+          text: $localize`Indkøbsform`,
         },
-        procurementStrategy: {
-          text: $localize`Genanskaffelsesstrategi`
+        sActive: {
+          text: $localize`Gyldig`,
         },
-        procurementPlan: {
-          text: $localize`Genanskaffelsesplan`
+        agreementPeriod: {
+          text: $localize`Gyldig fra/til`,
         },
-        procurementInitiated: {
-          text: $localize`Genanskaffelse igangsat`
+        notes: {
+          text: $localize`Bemærkning`,
         },
-        externalSigner: {
-          text: $localize`Leverandørs underskrift`,
-          helpText: "Herunder: 'Underskriver', 'Underskrevet' og 'Dato'"
+        parentContract: {
+          text: $localize`Overordnet kontrakt`,
         },
         internalSigner: {
           text: $localize`Kontraktunderskriver`,
-          helpText: "Herunder: 'Underskriver', 'Underskrevet' og 'Dato'"
+          helpText: "Herunder: 'Underskriver', 'Underskrevet' og 'Dato'",
         },
-        agreementPeriod: {
-          text: $localize`Gyldig fra/til`
+        externalSigner: {
+          text: $localize`Leverandørs underskrift`,
+          helpText: "Herunder: 'Underskriver', 'Underskrevet' og 'Dato'",
         },
-        isActive: {
-          text: $localize`Gyldig`
+        procurementStrategy: {
+          text: $localize`Genanskaffelsesstrategi`,
+        },
+        procurementPlan: {
+          text: $localize`Genanskaffelsesplan`,
+        },
+        procurementInitiated: {
+          text: $localize`Genanskaffelse igangsat`,
+        },
+        createdBy: {
+          text: $localize`Oprettet af`,
+        },
+        lastModifiedBy: {
+          text: $localize`Sidst redigeret af`,
+        },
+        lastModifiedDate: {
+          text: $localize`Sidst redigeret dato`,
         }
-      }
+      },
     },
     itSystems: {
       text: $localize`IT Systemer`,
       helpText: uiConfigHelpTexts.cannotChangeTabOnlyThroughModuleConfig,
-      isObligatory: true
     },
     dataProcessing: {
       text: $localize`Databehandling`,
       helpText: uiConfigHelpTexts.cannotChangeTabOnlyThroughModuleConfig,
-      isObligatory: true
     },
     deadlines: {
       text: $localize`Aftalefrister`,
@@ -69,13 +87,13 @@ export const ItContractsUiBluePrint = {
       children: {
         agreementDeadlines: {
           text: $localize`Aftalefrister`,
-          helpText: "Herunder: 'Varighed', 'Løbende', 'Option' forlæng', 'Antal brugte optioner' og 'Uopsigelig til' "
+          helpText: "Herunder: 'Varighed', 'Løbende', 'Option' forlæng', 'Antal brugte optioner' og 'Uopsigelig til' ",
         },
         termination: {
           text: $localize`Opsigelse`,
-          helpText: "Herunder: 'Kontrakten opsagt', 'Opsigelsesfrist', 'Løbende' og 'Inden udgangen af' "
-        }
-      }
+          helpText: "Herunder: 'Kontrakten opsagt', 'Opsigelsesfrist', 'Løbende' og 'Inden udgangen af' ",
+        },
+      },
     },
     economy: {
       text: $localize`Økonomi`,
@@ -84,31 +102,29 @@ export const ItContractsUiBluePrint = {
       children: {
         paymentModel: {
           text: $localize`Betalingsmodel`,
-          helpText: "Herunder: 'Driftsvederlag påbegyndt', 'Betalingsfrekvens', 'Betalingsmodel' og 'Prisregulering' "
+          helpText: "Herunder: 'Driftsvederlag påbegyndt', 'Betalingsfrekvens', 'Betalingsmodel' og 'Prisregulering' ",
         },
         extPayment: {
-          text: $localize`Ekstern betaling`
+          text: $localize`Ekstern betaling`,
         },
         intPayment: {
-          text: $localize`Intern betaling`
-        }
-      }
+          text: $localize`Intern betaling`,
+        },
+      },
     },
     contractRoles: {
-      text: $localize`Kontraktroller`
+      text: $localize`Kontraktroller`,
     },
     hierarchy: {
       text: $localize`Hierarki`,
       helpText: uiConfigHelpTexts.cannotChangeTab,
-      isObligatory: true
     },
     advice: {
-      text: $localize`Advis`
+      text: $localize`Advis`,
     },
     references: {
       text: $localize`Referencer`,
       helpText: uiConfigHelpTexts.cannotChangeTab,
-      isObligatory: true
-    }
-  }
+    },
+  },
 };
