cb05550ddcb8aa6685b2c8e4acc23b73bbf409a2
JacobBorchMadsen <jbma@strongminds.dk>
2024-12-11 16:04:22 +0100

    put alert state in the appropiate file

---
+++ src/app/store/alerts/state.ts
@@ -0,0 +1,31 @@
+import { EntityState } from '@ngrx/entity';
+import { APIAlertResponseDTO } from 'src/app/api/v2';
+
+export interface AlertsState {
+  alerts: { [key in RelatedEntityType]: EntityState<Alert> };
+  cacheTime: { [key in RelatedEntityType]: number | undefined };
+}
+
+export enum RelatedEntityType {
+  ItSystemUsage = 'ItSystemUsage',
+  ItContract = 'ItContract',
+  DataProcessingRegistration = 'DataProcessingRegistration',
+}
+
+export interface Alert {
+  uuid: string;
+  name?: string;
+  alertType: APIAlertResponseDTO.AlertTypeEnum;
+  message?: string;
+  created?: string;
+}
+
+export function adaptAlert(alert: APIAlertResponseDTO): Alert {
+  return {
+    uuid: alert.uuid,
+    name: alert.name,
+    alertType: alert.alertType ?? 'Advis', //The only alert type atm (11/12/2024)
+    message: alert.message,
+    created: alert.created,
+  };
+}
