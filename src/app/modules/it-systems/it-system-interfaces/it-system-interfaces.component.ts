294a2aad2bab73bca2cee7eb11135c59fd0208c8
JacobBorchMadsen <jbma@strongminds.dk>
2024-12-16 15:37:20 +0100

    remove unused import

--- src/app/modules/it-systems/it-system-interfaces/it-system-interfaces.component.ts
+++ src/app/modules/it-systems/it-system-interfaces/it-system-interfaces.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
