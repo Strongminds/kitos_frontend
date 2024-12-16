8c81befe32a0ed4af5b1150c7314fcd55bf9710b
JacobBorchMadsen <jbma@strongminds.dk>
2024-12-16 09:12:27 +0100

    cleanup solution for dropdown column data filter

--- src/app/shared/components/grid/dropdown-column-data-filter/dropdown-column-data-filter.component.ts
+++ src/app/shared/components/grid/dropdown-column-data-filter/dropdown-column-data-filter.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { Actions, ofType } from '@ngrx/effects';
-import { concatLatestFrom } from '@ngrx/operators';
+import { Actions } from '@ngrx/effects';
 import { ColumnComponent, FilterService } from '@progress/kendo-angular-grid';
 import { CompositeFilterDescriptor, FilterDescriptor } from '@progress/kendo-data-query';
 import { first, Observable } from 'rxjs';
 import { initializeApplyFilterSubscription } from 'src/app/shared/helpers/grid-filter.helpers';
 import { RegistrationEntityTypes } from 'src/app/shared/models/registrations/registration-entity-categories.model';
 import { ColumnFilterDataService, GridDataKey } from 'src/app/shared/services/column-filter-data.service';
-import { GridSavedFilterActions } from 'src/app/store/grid/actions';
 import { AppBaseFilterCellComponent } from '../app-base-filter-cell.component';
 import { DropdownFilterComponent, FilterDropdownOption } from '../dropdown-filter/dropdown-filter.component';
 
@@ -39,25 +37,12 @@ export class DropdownColumnDataFilterComponent extends AppBaseFilterCellComponen
     this.options$ = this.columnFilterDataService.get(this.serviceKey);
 
     const updateMethod: (filter: FilterDescriptor | undefined) => void = (filter) => {
-      this.actions$
-        .pipe(
-          ofType(GridSavedFilterActions.dropdownDataOptionsUpdated),
-          concatLatestFrom(() => this.options$),
-          first()
-        )
-        .subscribe(([payload, options]) => {
-          if (payload.column !== this.columnName || this.entityType !== payload.entityType) {
-            return;
-          }
-          const newValue = filter?.value;
-          const newOption = options.find((option) => option.value === newValue);
-          this.dropdownFilter.chosenOption = newOption;
-        });
+      this.options$.pipe(first()).subscribe((options) => {
+        const newValue = filter?.value;
+        const newOption = options.find((option) => option.value === newValue);
+        this.dropdownFilter.chosenOption = newOption;
+      });
     };
     initializeApplyFilterSubscription(this.actions$, this.entityType, this.column.field, updateMethod);
-  }
-
-  private getProperty<T, K extends keyof T>(obj: T, propName: K): T[K] {
-    return obj[propName];
   }
 }
