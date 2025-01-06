a0651f4d54212a1f6c340115c38f97e429bb34fb
NikolajNielsen <nan@strongminds.dk>
2025-01-06 10:56:22 +0100

    Make drop box cover more of expanded nodes

--- src/app/shared/components/drag-and-drop-tree/drag-and-drop-tree.component.ts
+++ src/app/shared/components/drag-and-drop-tree/drag-and-drop-tree.component.ts
@@ -59,10 +59,12 @@ export class DragAndDropTreeComponent<T> implements OnInit {
     };
     const targetListUuid = this.getParentNodeId(this.dropActionTodo.targetId, this.nodes, 'main');
     const targetRect = container.getBoundingClientRect();
-    const oneThird = targetRect.height / 3;
+    const oneThirdOfRectangle = targetRect.height / 3;
+    const oneSixthOfRectangle = targetRect.height / 6;
+    const pointerPositionYInRectangle = event.pointerPosition.y - targetRect.top;
 
     // before
-    if (event.pointerPosition.y - targetRect.top < oneThird) {
+    if (pointerPositionYInRectangle < oneSixthOfRectangle) {
       //if target uuid is main, it should be placed after the node
       if (targetListUuid === 'main') {
         this.dropActionTodo['action'] = 'inside';
@@ -71,7 +73,7 @@ export class DragAndDropTreeComponent<T> implements OnInit {
       }
     }
     // after
-    else if (event.pointerPosition.y - targetRect.top > 2 * oneThird) {
+    else if (pointerPositionYInRectangle > 2 * oneThirdOfRectangle) {
       this.dropActionTodo['action'] = 'after';
     }
     // inside
