21d4496d56d2217404b22c0c69936e0088e619a7
NikolajNielsen <nan@strongminds.dk>
2025-01-15 12:36:33 +0100

    api

--- src/app/api/v2/api/v2OrganizationUnitGlobalRoleOptionTypesInternalINTERNAL.service.ts
+++ src/app/api/v2/api/v2OrganizationUnitGlobalRoleOptionTypesInternalINTERNAL.service.ts
@@ -45,7 +45,7 @@ export interface PostSingleOrganizationUnitGlobalRoleOptionTypesInternalV2Create
 })
 export class APIV2OrganizationUnitGlobalRoleOptionTypesInternalINTERNALService {
 
-    protected basePath = 'https://localhost:44300';
+    protected basePath = 'https://kitos-dev.strongminds.dk';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
