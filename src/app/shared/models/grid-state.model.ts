30fa547d2082fa7a6a904fc7aaa87f78c8e2c7fc
JacobBorchMadsen <jbma@strongminds.dk>
2025-01-16 14:34:54 +0100

    add proper comment for default virtual page size

--- src/app/shared/models/grid-state.model.ts
+++ src/app/shared/models/grid-state.model.ts
@@ -5,7 +5,11 @@ export interface GridState extends State {
   all?: boolean;
 }
 
-export const DEFAULT_VIRTUALIZTION_PAGE_SIZE = 70; // Default page size for
+/**
+ * Recommended to at least be 3x times the amount of visible rows (source: https://www.telerik.com/kendo-angular-ui/components/grid/scroll-modes/virtual, 16/01/2025)
+ * Higher pagesizes for virtualization will result in less frequent but longer loads, and vice versa.
+ */
+export const DEFAULT_VIRTUALIZTION_PAGE_SIZE = 70;
 
 export const defaultODataGridState: GridState = {
   skip: 0,
