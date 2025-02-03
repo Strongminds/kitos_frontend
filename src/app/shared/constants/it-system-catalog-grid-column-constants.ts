7deb36c2e42be3ba538dbfbc4a9b3d510fea1733
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-03 09:45:40 +0100

    refactor catalog to use grid constants

---
+++ src/app/shared/constants/it-system-catalog-grid-column-constants.ts
@@ -0,0 +1,22 @@
+export const IS_IN_USE = "IsInUse";
+export const DISABLED = "Disabled";
+export const PARENT_NAME = "Parent.Name";
+export const PREVIOUS_NAME = "PreviousName";
+export const NAME = "Name";
+export const EXTERNAL_UUID = "ExternalUuid";
+export const ACCESS_MODIFIER = "AccessModifier";
+export const BUSINESS_TYPE_NAME = "BusinessType.Name";
+export const KLE_IDS = "KLEIds";
+export const KLE_NAMES = "KLENames";
+export const USAGES = "Usages";
+
+export const ORGANIZATION_NAME = "Organization.Name";
+export const LAST_CHANGED_BY_USER_NAME = "LastChangedByUser.Name";
+export const LAST_CHANGED = "LastChanged";
+export const REFERENCE_TITLE = "Reference.Title";
+export const REFERENCE_URL = "Reference.URL";
+export const REFERENCE_EXTERNAL_REFERENCE_ID = "Reference.ExternalReferenceId";
+export const UUID = "Uuid";
+export const DESCRIPTION = "Description";
+export const ARCHIVE_DUTY = "ArchiveDuty";
+export const ARCHIVE_DUTY_COMMENT = "ArchiveDutyComment";
