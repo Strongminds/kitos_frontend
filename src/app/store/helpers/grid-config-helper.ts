7d3ecb07f385838178171bb7f685961282c077f1
NikolajNielsen <nan@strongminds.dk>
2025-02-04 11:50:22 +0100

    Filter columns before returning

--- src/app/store/helpers/grid-config-helper.ts
+++ src/app/store/helpers/grid-config-helper.ts
@@ -2,11 +2,24 @@ import { APIColumnConfigurationResponseDTO } from 'src/app/api/v2';
 import { GridColumn } from 'src/app/shared/models/grid-column.model';
 
 export function getNewGridColumnsBasedOnConfig(
-  configColumns: APIColumnConfigurationResponseDTO[],
+  columnConfigs: APIColumnConfigurationResponseDTO[],
   columns: GridColumn[]
 ): GridColumn[] {
-  return columns.map((col) => ({
+  const columnsWithHiddenState = columns.map((col) => ({
     ...col,
-    hidden: !configColumns.some((configCol) => configCol.persistId === col.persistId),
+    hidden: !columnConfigs.some((config) => columnIsConfigTarget(col, config)),
   }));
+  return columnsWithHiddenState.sort((a, b) => {
+    const aIndex = getColumnIndexFromConfig(columnConfigs, a);
+    const bIndex = getColumnIndexFromConfig(columnConfigs, b);
+    return aIndex - bIndex;
+  });
+}
+
+function getColumnIndexFromConfig(columnConfigs: APIColumnConfigurationResponseDTO[], column: GridColumn) {
+  return columnConfigs.findIndex((config) => columnIsConfigTarget(column, config))
+}
+
+function columnIsConfigTarget(column: GridColumn, columnConfig: APIColumnConfigurationResponseDTO){
+  return column.persistId === columnConfig.persistId;
 }
