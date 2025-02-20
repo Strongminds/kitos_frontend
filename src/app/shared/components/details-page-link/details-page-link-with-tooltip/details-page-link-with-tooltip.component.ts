b6ea3459cb65630d9ff15c143c63e79df0c889ff
NikolajNielsen <nan@strongminds.dk>
2025-02-20 08:53:51 +0100

    Remove duplicate code

--- src/app/shared/components/details-page-link/details-page-link-with-tooltip/details-page-link-with-tooltip.component.ts
+++ src/app/shared/components/details-page-link/details-page-link-with-tooltip/details-page-link-with-tooltip.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { getDetailsPageLink } from 'src/app/shared/helpers/link.helpers';
+import { Component, Input } from '@angular/core';
 import { RegistrationEntityTypes } from 'src/app/shared/models/registrations/registration-entity-categories.model';
 import { LinkFontSizes } from 'src/app/shared/models/sizes/link-font-sizes.model';
 
@@ -8,7 +7,7 @@ import { LinkFontSizes } from 'src/app/shared/models/sizes/link-font-sizes.model
   templateUrl: './details-page-link-with-tooltip.component.html',
   styleUrl: './details-page-link-with-tooltip.component.scss'
 })
-export class DetailsPageLinkWithTooltipComponent implements OnInit{
+export class DetailsPageLinkWithTooltipComponent {
     @Input() public itemPath?: string;
     @Input() public linkFontSize: LinkFontSizes = 'medium';
     @Input() public itemType: RegistrationEntityTypes | undefined;
@@ -16,13 +15,4 @@ export class DetailsPageLinkWithTooltipComponent implements OnInit{
     @Input() public disableRedirect = false;
     @Input() public itemPathIncludesSubmodule = false;
     @Input() public toolTip!: string;
-
-    public detailsPageRouterPath: string | null = null;
-
-    public ngOnInit(): void {
-        const path = getDetailsPageLink(this.itemPath, this.itemType, this.subpagePath, this.itemPathIncludesSubmodule);
-        if (path) {
-          this.detailsPageRouterPath = path;
-        }
-      }
 }
