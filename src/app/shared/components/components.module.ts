17aaa134b66297a0973e6fa37e68582e6bf22c78
NikolajNielsen <nan@strongminds.dk>
2025-02-21 10:41:20 +0100

    Fix merge

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -169,7 +169,6 @@ import { SlideToggleComponent } from './slide-toggle/slide-toggle.component';
 import { SnackbarActionsComponent } from './snackbar-actions/snackbar-actions.component';
 import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 import { VerticalContentGridSectionMarginLeftComponent } from './vertical-content-grid-section-margin-left/vertical-content-grid-section-margin-left.component';
-import { ReadonlyLinkTextboxComponent } from './link-textbox/readonly-link-textbox/readonly-link-textbox.component';
 import { TextCellComponent } from './grid/grid-cell/cell-types/text-cell/text-cell.component';
 import { StatusCellComponent } from './grid/grid-cell/cell-types/status-cell/status-cell.component';
 import { ThousandSeperatorCellComponent } from './grid/grid-cell/cell-types/thousand-seperator-cell/thousand-seperator-cell.component';
