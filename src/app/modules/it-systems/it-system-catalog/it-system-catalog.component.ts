1db6ba14b17b4e4e904967eeea58dc9af97cfc67
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-24 13:22:20 +0100

    make new fields visible in grid, and add textboxes in catalog page

--- src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
+++ src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
@@ -213,6 +213,18 @@ export class ItSystemCatalogComponent extends BaseOverviewComponent implements O
       section: ARCHIVE_SECTION_NAME,
       hidden: true,
     },
+    {
+      field: CatalogFields.DBS_SYSTEM_NAME,
+      title: $localize`DBS Systemnavn`,
+      section: this.systemSectionName,
+      hidden: true,
+    },
+    {
+      field: CatalogFields.DBS_DATA_PROCESSOR_NAME,
+      title: $localize`DBS Databehandlernavn`,
+      section: this.systemSectionName,
+      hidden: true,
+    },
   ];
 
   constructor(
