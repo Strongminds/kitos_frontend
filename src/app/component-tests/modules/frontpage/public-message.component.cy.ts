cdd1445cf2b451e14533f9ea38e30e3a8f781ae1
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-26 09:47:48 +0100

    mount component in test

--- src/app/component-tests/modules/frontpage/public-message.component.cy.ts
+++ src/app/component-tests/modules/frontpage/public-message.component.cy.ts
@@ -1,5 +1,7 @@
+import { PublicMessageComponent } from 'src/app/modules/frontpage/public-message/public-message.component';
+
 describe('PublicMessageComponent', () => {
   it('Can mount', () => {
-    cy.mount()
+    cy.mount(PublicMessageComponent);
   });
 });
