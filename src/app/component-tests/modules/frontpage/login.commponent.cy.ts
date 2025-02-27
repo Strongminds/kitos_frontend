275f608d13d418789b8efaf975a6fa46559a1896
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-27 16:48:33 +0100

    fix component test

--- src/app/component-tests/modules/frontpage/login.commponent.cy.ts
+++
@@ -1,6 +0,0 @@
-import { FrontpageModule } from 'src/app/modules/frontpage/frontpage.module';
-import { LoginV2Component } from 'src/app/modules/frontpage/login-v2/login-v2.component';
-
-it('Can see basic elements', () => {
-  cy.mount(LoginV2Component, { imports: [FrontpageModule] });
-});
--- src/app/component-tests/modules/frontpage/public-message.component.cy.ts
+++ src/app/component-tests/modules/frontpage/public-message.component.cy.ts
@@ -1,6 +1,10 @@
-import { FrontpageModule } from 'src/app/modules/frontpage/frontpage.module';
 import { PublicMessageComponent } from 'src/app/modules/frontpage/public-message/public-message.component';
 import { PublicMessage } from 'src/app/shared/models/public-message.model';
+import { of } from 'rxjs';
+import { FrontpageComponentStore } from 'src/app/modules/frontpage/frontpage.component-store';
+import { mapStatusType } from 'src/app/shared/models/status-type.model';
+import { APIPublicMessageRequestDTO } from 'src/app/api/v2';
+import { FrontpageModule } from 'src/app/modules/frontpage/frontpage.module';
 
 describe('PublicMessageComponent', () => {
   const examplePublicMessage: PublicMessage = {
@@ -12,36 +16,43 @@ describe('PublicMessageComponent', () => {
   it('Can see title, short description and icon', () => {
     mountComponent(examplePublicMessage);
 
-    cy.contains(examplePublicMessage.title!);
-    cy.contains(examplePublicMessage.shortDescription!);
+    cy.contains(examplePublicMessage.title as string);
+    cy.contains(examplePublicMessage.shortDescription as string);
     cy.get('app-document-icon').should('exist');
   });
 
   it('Has active status chip when active', () => {
-    mountComponent({ ...examplePublicMessage, status: true });
-
+    mountComponent({ ...examplePublicMessage, status: mapStatusType(APIPublicMessageRequestDTO.StatusEnum.Active) });
     cy.get('app-status-chip').contains('Normal drift');
   });
 
   it('Has inactive status chip when not active', () => {
-    mountComponent({ ...examplePublicMessage, status: false });
-
+    mountComponent({ ...examplePublicMessage, status: mapStatusType(APIPublicMessageRequestDTO.StatusEnum.Inactive) });
     cy.get('app-status-chip').contains('Ustabil drift');
   });
 
   it('Has no status chip when status is undefined', () => {
-    mountComponent(examplePublicMessage);
-
-    cy.get('app-chip').should('not.exist');
+    mountComponent({ ...examplePublicMessage, status: undefined });
+    cy.get('app-status-chip').should('not.exist');
   });
 });
 
 function mountComponent(publicMessage: PublicMessage) {
   cy.mount(PublicMessageComponent, {
     componentProperties: {
-      publicMessage,
-      iconType: 'document',
+      config: {
+        iconType: 'document',
+        index: 0,
+      },
+      mode: 'normal',
     },
+    providers: [
+      {
+        provide: FrontpageComponentStore,
+        useValue: { publicMessages$: of([publicMessage]) },
+      },
+    ],
+
     imports: [FrontpageModule],
   });
 }
