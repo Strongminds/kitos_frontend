7c25dbb2722dad8b8f80a595d7e539b9e3d747a0
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-28 13:01:33 +0100

    rename model names from dbs to legal

--- cypress/e2e/it-system-usage/details-pages/it-system-usage.front-page.cy.ts
+++ cypress/e2e/it-system-usage/details-pages/it-system-usage.front-page.cy.ts
@@ -39,8 +39,8 @@ describe('it-system-usage', () => {
 
     // Test parent system deactivated
     cy.input('Overordnet system').should('have.value', 'System 3 (ikke tilgængeligt)');
-    cy.getByDataCy('dbs-name').find('input').should('have.value', 'DBS navn 123');
-    cy.getByDataCy('dbs-data-processor-name').find('input').should('have.value', 'DBS databehandler navn 123');
+    cy.getByDataCy('legal-name').find('input').should('have.value', 'DBS navn 123');
+    cy.getByDataCy('legal-data-processor-name').find('input').should('have.value', 'DBS databehandler navn 123');
 
     // Test obselete option
     cy.dropdown('Forretningstype').should('have.text', 'Test (udgået)');
