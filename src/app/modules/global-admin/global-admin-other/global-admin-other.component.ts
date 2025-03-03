b5dbb56b1f0719006df2b9d9d8427f843f08afe3
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-03 12:39:11 +0100

    introduce segment for global admin others page

--- src/app/modules/global-admin/global-admin-other/global-admin-other.component.ts
+++ src/app/modules/global-admin/global-admin-other/global-admin-other.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export enum GlobalAdminOtherSegmentOptionType {
+  UserManagement = 'UserManagement',
+  APIUsers = 'APIUsers',
+  Misc = 'Misc',
+}
+
 @Component({
   selector: 'app-global-admin-other',
   templateUrl: './global-admin-other.component.html',
-  styleUrl: './global-admin-other.component.scss'
+  styleUrl: './global-admin-other.component.scss',
 })
 export class GlobalAdminOtherComponent {
+  public readonly SegmentType = GlobalAdminOtherSegmentOptionType;
+  public selectedSegment = GlobalAdminOtherSegmentOptionType.UserManagement;
 
+  public readonly segmentOptions = [
+    { text: $localize`Brugerstyring`, value: GlobalAdminOtherSegmentOptionType.UserManagement },
+    { text: $localize`API-brugere`, value: GlobalAdminOtherSegmentOptionType.APIUsers },
+    { text: $localize`Diverse`, value: GlobalAdminOtherSegmentOptionType.Misc },
+  ];
 }
