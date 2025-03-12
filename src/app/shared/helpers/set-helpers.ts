2fb2986f698063aac2d73efe9ffd8d0de9dd40ff
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-12 13:53:13 +0100

    export the new set helper method

--- src/app/shared/helpers/set-helpers.ts
+++ src/app/shared/helpers/set-helpers.ts
@@ -1,4 +1,4 @@
-function areSetsEqual<T>(a: Set<T>, b: Set<T>): boolean {
+export function areSetsEqual<T>(a: Set<T>, b: Set<T>): boolean {
   if (a.size !== b.size) return false;
   for (const item of a) {
     if (!b.has(item)) return false;
