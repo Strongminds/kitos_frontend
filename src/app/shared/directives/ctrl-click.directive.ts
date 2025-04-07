649d564b6f3af2842d70637e32dda5810840d52d
JacobBorchMadsen <jbma@strongminds.dk>
2025-04-07 11:37:53 +0200

    Refactor CtrlClickDirective to consolidate URL opening logic into a separate method

--- src/app/shared/directives/ctrl-click.directive.ts
+++ src/app/shared/directives/ctrl-click.directive.ts
@@ -9,30 +9,28 @@ export class CtrlClickDirective {
 
   @HostListener('click', ['$event'])
   onClick(event: MouseEvent) {
-    console.log('CtrlClickDirective clicked', event);
-    if (!this.routerLink) {
-      return;
-    }
-
-    if (event.ctrlKey || event.metaKey || event.button === 1) {
-      const relativeUrl = this.router.serializeUrl(this.routerLink.urlTree ?? new UrlTree());
-      const fullUrl = window.location.origin + relativeUrl;
-
-      window.open(fullUrl, '_blank');
-
-      event.preventDefault();
-      event.stopImmediatePropagation();
+    if (event.ctrlKey) {
+      this.openUrlInNewTab(event);
     }
   }
 
   @HostListener('auxclick', ['$event'])
   onAuxClick(event: MouseEvent) {
-    if (event.button === 1 && this.routerLink) {
-      const relativeUrl = this.router.serializeUrl(this.routerLink.urlTree ?? new UrlTree());
-      const fullUrl = window.location.origin + relativeUrl;
-      window.open(fullUrl, '_blank');
-      event.preventDefault();
-      event.stopImmediatePropagation();
+    if (event.button === 1) {
+      this.openUrlInNewTab(event);
     }
+  }
+
+  private openUrlInNewTab(event: MouseEvent): void {
+    if (!this.routerLink) {
+      return;
+    }
+
+    const relativeUrl = this.router.serializeUrl(this.routerLink.urlTree ?? new UrlTree());
+    const fullUrl = window.location.origin + relativeUrl;
+
+    window.open(fullUrl, '_blank');
+    event.preventDefault();
+    event.stopImmediatePropagation();
   }
 }
