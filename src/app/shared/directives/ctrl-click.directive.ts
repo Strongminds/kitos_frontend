b68223f105f6a1f3d70992fb106f8642f5f85b73
JacobBorchMadsen <jbma@strongminds.dk>
2025-04-08 12:00:01 +0200

    Refactor link-textbox component and CtrlClickDirective for improved functionality and event handling

--- src/app/shared/directives/ctrl-click.directive.ts
+++ src/app/shared/directives/ctrl-click.directive.ts
@@ -12,16 +12,18 @@ export class CtrlClickDirective implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.captureClickListener = this.onCaptureClick.bind(this);
     this.el.nativeElement.addEventListener('click', this.captureClickListener, true);
+    this.el.nativeElement.addEventListener('auxclick', this.captureClickListener, true);
   }
 
   ngOnDestroy(): void {
     this.el.nativeElement.removeEventListener('click', this.captureClickListener, true);
+    this.el.nativeElement.removeEventListener('auxclick', this.captureClickListener, true);
   }
 
   private onCaptureClick(event: MouseEvent): void {
     if (!this.routerLink) return;
 
-    if (!event.ctrlKey) return;
+    if (!event.ctrlKey && event.button !== 1) return;
 
     const relativeUrl = this.router.serializeUrl(this.routerLink.urlTree ?? new UrlTree());
     const fullUrl = window.location.origin + relativeUrl;
