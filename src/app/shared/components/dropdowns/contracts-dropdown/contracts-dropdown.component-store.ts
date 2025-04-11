84eb0d5f029b88b0d09f5bc7aceac1da08d33082
Aleksander Naskret <aln@strongminds.dk>
2025-04-11 13:07:16 +0200

    exclude contracts from other orgs

--- src/app/shared/components/dropdowns/contracts-dropdown/contracts-dropdown.component-store.ts
+++ src/app/shared/components/dropdowns/contracts-dropdown/contracts-dropdown.component-store.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { tapResponse } from '@ngrx/operators';
-import { mergeMap, Observable, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { combineLatestWith, mergeMap, Observable, tap } from 'rxjs';
 import { APIItContractResponseDTO, APIV2ItContractService } from 'src/app/api/v2';
+import { selectOrganizationUuid } from 'src/app/store/user-store/selectors';
 
 interface State {
   contracts: APIItContractResponseDTO[];
@@ -14,7 +16,10 @@ export class ContractDropdownComponentStore extends ComponentStore<State> {
   public readonly contracts$ = this.select((state) => state.contracts);
   public readonly loading$ = this.select((state) => state.loading);
 
-  constructor(@Inject(APIV2ItContractService) private readonly itContractService: APIV2ItContractService) {
+  constructor(
+    @Inject(APIV2ItContractService) private readonly itContractService: APIV2ItContractService,
+    private readonly store: Store
+  ) {
     super({ contracts: [], loading: false });
   }
 
@@ -35,8 +40,9 @@ export class ContractDropdownComponentStore extends ComponentStore<State> {
   public searchContracts = this.effect((search$: Observable<string | undefined>) =>
     search$.pipe(
       tap(() => this.setLoading(true)),
-      mergeMap((search) => {
-        return this.itContractService.getManyItContractV2GetItContracts({ nameContent: search }).pipe(
+      combineLatestWith(this.store.select(selectOrganizationUuid)),
+      mergeMap(([search, organizationUuid]) => {
+        return this.itContractService.getManyItContractV2GetItContracts({ nameContent: search, organizationUuid }).pipe(
           tapResponse(
             (contracts) => this.setContracts(contracts),
             (error) => console.error(error),
