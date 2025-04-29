ac08e6602a49cec6b962ab4f35668495ecffd9bd
JacobBorchMadsen <jbma@strongminds.dk>
2025-04-29 16:09:35 +0200

    make console error go away for user edit dialog when user has roles

--- src/app/modules/organization/organization-users/edit-user-dialog/edit-user-dialog.component.ts
+++ src/app/modules/organization/organization-users/edit-user-dialog/edit-user-dialog.component.ts
@@ -32,13 +32,13 @@ import { OrganizationUserActions } from 'src/app/store/organization/organization
 import { BaseUserDialogComponent } from '../base-user-dialog.component';
 
 @Component({
-    selector: 'app-edit-user-dialog',
-    templateUrl: './edit-user-dialog.component.html',
-    styleUrl: './edit-user-dialog.component.scss',
-    providers: [CreateUserDialogComponentStore],
-    standalone: false
+  selector: 'app-edit-user-dialog',
+  templateUrl: './edit-user-dialog.component.html',
+  styleUrl: './edit-user-dialog.component.scss',
+  providers: [CreateUserDialogComponentStore],
+  standalone: false,
 })
-export class EditUserDialogComponent extends BaseUserDialogComponent implements OnInit, AfterViewInit {
+export class EditUserDialogComponent extends BaseUserDialogComponent implements OnInit {
   @Input() public user!: ODataOrganizationUser;
   @Input() public isNested!: boolean;
   @ViewChild(MultiSelectDropdownComponent)
@@ -105,14 +105,15 @@ export class EditUserDialogComponent extends BaseUserDialogComponent implements 
         }
       })
     );
+    const initialValues = this.getUserRoleChoices();
+    this.selectedRoles = initialValues.map((role) => role.value);
   }
 
-  public ngAfterViewInit(): void {
+  public getUserRoleChoices(): UserRoleChoice[] {
     const initialValues = this.getSelectableRolesThatUserHas()
       .map((role) => mapUserRoleChoice(role))
       .filter((role) => role !== undefined);
-    this.multiSelectDropdown.setValues(initialValues);
-    this.selectedRoles = initialValues.map((role) => role.value);
+    return initialValues;
   }
 
   public onSave(): void {
