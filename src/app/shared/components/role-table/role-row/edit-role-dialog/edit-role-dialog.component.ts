2959df78725f20bf44d04b53f1fa50f321abdf52
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-01 11:55:06 +0200

    implement action dispatches for adding and removing roles to mimick an edit

--- src/app/shared/components/role-table/role-row/edit-role-dialog/edit-role-dialog.component.ts
+++ src/app/shared/components/role-table/role-row/edit-role-dialog/edit-role-dialog.component.ts
@@ -6,6 +6,7 @@ import { APIRegularOptionResponseDTO } from 'src/app/api/v2';
 import { RoleAssignment } from 'src/app/shared/models/helpers/read-model-role-assignments';
 import { RoleOptionTypes } from 'src/app/shared/models/options/role-option-types.model';
 import { ShallowUser } from 'src/app/shared/models/userV2.model';
+import { RoleOptionTypeService } from 'src/app/shared/services/role-option-type.service';
 
 @Component({
   selector: 'app-edit-role-dialog',
@@ -16,13 +17,17 @@ import { ShallowUser } from 'src/app/shared/models/userV2.model';
 export class EditRoleDialogComponent implements OnInit {
   @Input() public roleType!: RoleOptionTypes;
   @Input() public initialValue!: RoleAssignment;
+  @Input() public entityUuid!: string;
 
   public readonly formGroup = new FormGroup({
     role: new FormControl<APIRegularOptionResponseDTO | undefined>(undefined, Validators.required),
     user: new FormControl<ShallowUser | undefined>(undefined, Validators.required),
   });
 
-  constructor(private readonly store: Store, private readonly dialogRef: MatDialogRef<EditRoleDialogComponent>) {}
+  constructor(
+    private readonly dialogRef: MatDialogRef<EditRoleDialogComponent>,
+    private readonly roleOptionService: RoleOptionTypeService
+  ) {}
 
   ngOnInit(): void {
     this.formGroup.patchValue({
@@ -35,8 +40,10 @@ export class EditRoleDialogComponent implements OnInit {
     const formControls = this.formGroup.controls;
     const user = formControls.user.value;
     const role = formControls.role.value;
-    console.log('user: ', user);
-    console.log('role: ', role);
+    if (!user || !role) throw new Error('User or role is undefined');
+
+    this.roleOptionService.dispatchRemoveEntityRoleAction(this.initialValue, this.roleType);
+    this.roleOptionService.dispatchAddEntityRoleAction([user.uuid], role.uuid, this.roleType);
 
     this.dialogRef.close();
   }
