36f99aad7db08bb4a0052bb7e70c74e5e66e6d4f
Aleksander Naskret <aln@strongminds.dk>
2025-05-02 08:18:15 +0200

    remove unused modules

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -10,23 +10,22 @@ import { MenuItemComponent } from './menu-item/menu-item.component';
 import { MenuComponent } from './menu/menu.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotificationsButtonComponent } from './notifications-button/notifications-button.component';
-import { DirectivesModule } from 'src/app/shared/directives/directives.module';
+
 
 @NgModule({
     imports: [
-        CommonModule,
-        RouterModule,
-        MatToolbarModule,
-        MatBadgeModule,
-        MatMenuModule,
-        SharedModule,
-        DirectivesModule,
-        NavBarComponent,
-        MenuComponent,
-        MenuItemComponent,
-        ChooseOrganizationComponent,
-        NotificationsButtonComponent,
-    ],
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatMenuModule,
+    SharedModule,
+    NavBarComponent,
+    MenuComponent,
+    MenuItemComponent,
+    ChooseOrganizationComponent,
+    NotificationsButtonComponent,
+],
     exports: [NavBarComponent],
 })
 export class LayoutModule {}
