200a4b3ccf6102148392b7dccf664ff24b295440
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-06 09:38:50 +0200

    fix: make sure you can add a role, to a unit, even if it exists in a subunit

--- src/app/shared/components/role-table/role-table.create-dialog/role-table.create-dialog.component.ts
+++ src/app/shared/components/role-table/role-table.create-dialog/role-table.create-dialog.component.ts
@@ -127,6 +127,9 @@ export class RoleTableCreateDialogComponent extends BaseComponent implements OnI
         existingUserUuids = [];
         this.roleUserUuidsDictionary[roleUuid] = existingUserUuids;
       }
+      if (role.unitUuid && role.unitUuid != this.entityUuid) {
+        return;
+      }
       existingUserUuids.push(role.assignment.user.uuid);
     });
   }
