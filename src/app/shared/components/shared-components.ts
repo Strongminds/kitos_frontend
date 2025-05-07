f4ca07494f36f5851adfa4b164b774ea3f777072
Aleksander Naskret <aln@strongminds.dk>
2025-05-07 11:18:08 +0200

    align tooltip on icon-buttons

--- src/app/shared/components/shared-components.ts
+++ src/app/shared/components/shared-components.ts
@@ -1,12 +1,12 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DatePickerComponent } from '@progress/kendo-angular-dateinputs';
-import { TextBoxComponent } from '@progress/kendo-angular-inputs';
 import { ButtonComponent } from './buttons/button/button.component';
+import { DatePickerComponent } from './datepicker/datepicker.component';
 import { DropdownComponent } from './dropdowns/dropdown/dropdown.component';
 import { MultiSelectDropdownComponent } from './dropdowns/multi-select-dropdown/multi-select-dropdown.component';
 import { UserDropdownComponent } from './dropdowns/user-dropdown/user-dropdown.component';
 import { ParagraphComponent } from './paragraph/paragraph.component';
 import { TextBoxInfoComponent } from './textbox-info/textbox-info.component';
+import { TextBoxComponent } from './textbox/textbox.component';
 
 export const sharedFormComponents = [
   ButtonComponent,
