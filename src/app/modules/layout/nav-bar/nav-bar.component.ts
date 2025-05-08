b06188082879593b74d2bd9b63935498220cd225
NikolajNielsen <nan@strongminds.dk>
2025-05-08 10:52:12 +0200

    Add coloured nav bar on staging

--- src/app/modules/layout/nav-bar/nav-bar.component.ts
+++ src/app/modules/layout/nav-bar/nav-bar.component.ts
@@ -1,10 +1,14 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatToolbar } from '@angular/material/toolbar';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { combineLatest, filter, tap } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
+import { STAGING_ENVIRONMENT_NAME } from 'src/app/shared/constants/constants';
 import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { AppRootUrlResolverService } from 'src/app/shared/services/app-root-url-resolver.service';
 import { selectAllAlertCount } from 'src/app/store/alerts/selectors';
 import { OrganizationActions } from 'src/app/store/organization/actions';
 import { selectUIRootConfig } from 'src/app/store/organization/selectors';
@@ -15,16 +19,14 @@ import {
   selectUser,
   selectUserIsCurrentlyLocalAdmin,
 } from 'src/app/store/user-store/selectors';
+import { LogoutIconComponent } from '../../../shared/components/icons/logout-icon.component';
+import { TableIconComponent } from '../../../shared/components/icons/table-icon.component';
+import { SpacerComponent } from '../../../shared/components/spacer/spacer.component';
 import { AppPath } from '../../../shared/enums/app-path';
 import { ChooseOrganizationComponent } from '../choose-organization/choose-organization.component';
-import { MatToolbar } from '@angular/material/toolbar';
-import { NgIf, AsyncPipe } from '@angular/common';
-import { SpacerComponent } from '../../../shared/components/spacer/spacer.component';
-import { MenuComponent } from '../menu/menu.component';
 import { MenuItemComponent } from '../menu-item/menu-item.component';
-import { TableIconComponent } from '../../../shared/components/icons/table-icon.component';
+import { MenuComponent } from '../menu/menu.component';
 import { NotificationsButtonComponent } from '../notifications-button/notifications-button.component';
-import { LogoutIconComponent } from '../../../shared/components/icons/logout-icon.component';
 
 @Component({
   selector: 'app-nav-bar',
@@ -53,17 +55,24 @@ export class NavBarComponent extends BaseComponent implements OnInit {
   public readonly isUserCurrentyLocalAdmin$ = this.store.select(selectUserIsCurrentlyLocalAdmin);
 
   public readonly alertsCount$ = this.store.select(selectAllAlertCount);
+  public rootUrl: string;
 
   constructor(
     private store: Store,
     private dialog: MatDialog,
     private router: Router,
+    private rootUrlResolver: AppRootUrlResolverService
   ) {
     super();
+    this.rootUrl = this.rootUrlResolver.resolveRootUrl();
   }
 
   ngOnInit(): void {
     this.setupGetUIRootConfigOnNavigation();
+  }
+
+  public useStagingNavBarBackground() {
+    return this.rootUrl.includes(STAGING_ENVIRONMENT_NAME);
   }
 
   private setupGetUIRootConfigOnNavigation() {
@@ -71,9 +80,9 @@ export class NavBarComponent extends BaseComponent implements OnInit {
       combineLatest([this.user$, this.router.events])
         .pipe(
           filter(([user, event]) => user !== undefined && event instanceof NavigationEnd),
-          tap(() => this.store.dispatch(OrganizationActions.getUIRootConfig())),
+          tap(() => this.store.dispatch(OrganizationActions.getUIRootConfig()))
         )
-        .subscribe(),
+        .subscribe()
     );
   }
 
