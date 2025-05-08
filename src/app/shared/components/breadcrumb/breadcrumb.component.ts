cdd24b9e9702b68e0a989ea7395ff2602aa24355
Aleksander Naskret <aln@strongminds.dk>
2025-05-08 12:23:14 +0200

    fix other links

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,15 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { BreadCrumbContext } from '../../models/breadcrumbs/breadcrumb-context.model';
 import { BreadCrumb } from '../../models/breadcrumbs/breadcrumb.model';
-import { NgClass, NgIf } from '@angular/common';
 import { ChevronRightIconComponent } from '../icons/chevron-right-icon.component';
 
 @Component({
   selector: 'app-breadcrumb[breadCrumb][context]',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss'],
-  imports: [NgClass, NgIf, ChevronRightIconComponent],
+  imports: [CommonModule, ChevronRightIconComponent],
 })
 export class BreadcrumbComponent {
   @Input() public breadCrumb!: BreadCrumb;
@@ -17,10 +17,16 @@ export class BreadcrumbComponent {
 
   constructor(private router: Router) {}
 
-  public itemClicked() {
+  public itemClicked(event: MouseEvent) {
     const commands = this.context.routerCommands;
-    if (commands) {
-      this.router.navigate(commands);
+    if (!commands) return;
+
+    if (event.ctrlKey || event.button === 1) {
+      const url = this.router.serializeUrl(this.router.createUrlTree(commands));
+      window.open(url, '_blank');
+      return;
     }
+
+    this.router.navigate(commands);
   }
 }
