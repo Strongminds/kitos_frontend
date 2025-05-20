accd4aebca5e72dd379557f78da6cdef713cfd33
NikolajNielsen <nan@strongminds.dk>
2025-05-20 19:12:46 +0200

    Consolidate first test file with manual setup

--- cypress/e2e/data-processings/details-pages/data-processing.front-page.cy.ts
+++ cypress/e2e/data-processings/details-pages/data-processing.front-page.cy.ts
@@ -1,42 +1,42 @@
 /// <reference types="Cypress" />
 
 describe('data-processing-front-page', () => {
-  beforeEach(() => {
+  const setupTest = () => {
     cy.requireIntercept();
     cy.setupDataProcessingIntercepts();
 
     cy.intercept('PATCH', 'api/v2/data-processing-registrations/*', {
       fixture: './dpr/data-processing-registration-patch.json',
     });
-  });
+  }
 
-  it('Agreement conclusion date is enabled when agreement is concluded', () => {
+  it.only('Tests', () => {
+
+    setupTest();
     cy.setup(true, 'data-processing');
     cy.contains('Dpa 1').click();
 
     cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('be.disabled');
     cy.dropdownByCy('dpr-agreement-concluded', 'Ja', true);
     cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('not.be.disabled');
-  });
 
-  it('Can show responsible unit on startup', () => {
-    cy.setup(true, 'data-processing');
+    setupTest();
+     cy.setup(true, 'data-processing');
     const responsibleUnit = { name: 'En enhed', uuid: '0c2c1b3b-0b1b-4b3b-8b3b-0b1b3b0b1b3b' };
     cy.intercept('api/v2/organizations/*/organization-units?pageSize=*', { body: [responsibleUnit] });
     cy.setup(true, 'data-processing');
     cy.contains('Dpa 1').click();
 
     cy.contains('En enhed').should('exist');
-  });
 
-  it('Can not see responsible unit dropdown, if disabled by UI customization', () => {
+
+    setupTest();
     cy.setup(true, 'data-processing', './shared/ui-customization/dpr-responsible-unit-disabled.json');
     cy.contains('Dpa 1').click().wait(500);
 
     cy.getByDataCy('responsible-unit-select').should('not.exist');
-  });
 
-  it('Can patch and clear responsible unit', () => {
+    setupTest();
     cy.setup(true, 'data-processing');
     const responsibleUnitToSelect = {
       parentOrganizationUnit: null,
@@ -90,5 +90,88 @@ describe('data-processing-front-page', () => {
     cy.getByDataCy('responsible-unit-select').find('.ng-clear-wrapper').click();
 
     cy.contains(responsibleUnitToSelect.name).should('not.exist');
-  });
+
+  })
+
+  // it('Agreement conclusion date is enabled when agreement is concluded', () => {
+  //   cy.setup(true, 'data-processing');
+  //   cy.contains('Dpa 1').click();
+
+  //   cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('be.disabled');
+  //   cy.dropdownByCy('dpr-agreement-concluded', 'Ja', true);
+  //   cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('not.be.disabled');
+  // });
+
+  // it('Can show responsible unit on startup', () => {
+  //   cy.setup(true, 'data-processing');
+  //   const responsibleUnit = { name: 'En enhed', uuid: '0c2c1b3b-0b1b-4b3b-8b3b-0b1b3b0b1b3b' };
+  //   cy.intercept('api/v2/organizations/*/organization-units?pageSize=*', { body: [responsibleUnit] });
+  //   cy.setup(true, 'data-processing');
+  //   cy.contains('Dpa 1').click();
+
+  //   cy.contains('En enhed').should('exist');
+  // });
+
+  // it('Can not see responsible unit dropdown, if disabled by UI customization', () => {
+  //   cy.setup(true, 'data-processing', './shared/ui-customization/dpr-responsible-unit-disabled.json');
+  //   cy.contains('Dpa 1').click().wait(500);
+
+  //   cy.getByDataCy('responsible-unit-select').should('not.exist');
+  // });
+
+  // it('Can patch and clear responsible unit', () => {
+  //   cy.setup(true, 'data-processing');
+  //   const responsibleUnitToSelect = {
+  //     parentOrganizationUnit: null,
+  //     ean: null,
+  //     unitId: null,
+  //     origin: 'Kitos',
+  //     uuid: '0c2c1b3b-0b1b-4b3b-8b3b-0b1b3b0b1b3b',
+  //     name: 'En anden enhed',
+  //   };
+  //   const existingUnit = {
+  //     parentOrganizationUnit: null,
+  //     ean: null,
+  //     unitId: null,
+  //     origin: 'Kitos',
+  //     uuid: '0c2c1b3b-0b1b-4b3b-8b3b-0b1b3b0b1aaa',
+  //     name: 'En enhed',
+  //   };
+  //   cy.intercept('api/v2/organizations/*/organization-units?pageSize=*', {
+  //     body: [responsibleUnitToSelect, existingUnit],
+  //   });
+  //   cy.contains('Dpa 1').click();
+
+  //   cy.intercept('PATCH', 'api/v2/data-processing-registrations/*', (req) => {
+  //     expect(req.body.general.responsibleOrganizationUnitUuid).to.be.equal(responsibleUnitToSelect.uuid);
+  //     req.reply({ body: { general: { responsibleOrganizationUnit: responsibleUnitToSelect } } });
+  //   }).as('patchResponsibleUnit');
+
+  //   cy.getByDataCy('responsible-unit-select').click();
+  //   cy.contains(responsibleUnitToSelect.name).click();
+
+  //   cy.contains(responsibleUnitToSelect.name).should('exist');
+
+  //   cy.get('app-popup-message').should('exist');
+
+  //   cy.intercept('PATCH', 'api/v2/data-processing-registrations/*', (req) => {
+  //     cy.setup(true, 'data-processing');
+  //     expect(req.body.general.responsibleOrganizationUnitUuid).to.be.equal(null);
+  //     req.reply({ body: { general: { responsibleOrganizationUnit: null } } });
+  //   }).as('patchResponsibleUnit');
+
+  //   cy.contains(responsibleUnitToSelect.name).should('exist');
+
+  //   cy.get('app-popup-message').should('exist');
+
+  //   cy.intercept('PATCH', 'api/v2/data-processing-registrations/*', (req) => {
+  //     cy.setup(true, 'data-processing');
+  //     expect(req.body.general.responsibleOrganizationUnitUuid).to.be.equal(null);
+  //     req.reply({ body: { general: { responsibleOrganizationUnit: null } } });
+  //   }).as('patchResponsibleUnit');
+
+  //   cy.getByDataCy('responsible-unit-select').find('.ng-clear-wrapper').click();
+
+  //   cy.contains(responsibleUnitToSelect.name).should('not.exist');
+  // });
 });
