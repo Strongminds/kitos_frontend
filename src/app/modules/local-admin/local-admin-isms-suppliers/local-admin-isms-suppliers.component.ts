dcad798f8a7ab4fc9a8d28916494e673b5ac29a4
NikolajNielsen <nan@strongminds.dk>
2025-09-23 13:01:00 +0200

    Add confirm removal dialog

--- src/app/modules/local-admin/local-admin-isms-suppliers/local-admin-isms-suppliers.component.ts
+++ src/app/modules/local-admin/local-admin-isms-suppliers/local-admin-isms-suppliers.component.ts
@@ -1,39 +1,57 @@
 import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { first } from 'rxjs';
+import { BaseComponent } from 'src/app/shared/base/base.component';
 import { CardComponent } from 'src/app/shared/components/card/card.component';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/dialogs/confirmation-dialog/confirmation-dialog.component';
 import { LocalGridComponent } from 'src/app/shared/components/local-grid/local-grid.component';
 import { createGridActionColumn } from 'src/app/shared/models/grid-action-column.model';
+import { OrganizationActions } from 'src/app/store/organization/actions';
 import { selectOrganizationHasModifyPermission } from 'src/app/store/organization/selectors';
 
 @Component({
   selector: 'app-local-admin-isms-suppliers',
   imports: [CardComponent, LocalGridComponent, NgFor, AsyncPipe],
   templateUrl: './local-admin-isms-suppliers.component.html',
-  styleUrl: './local-admin-isms-suppliers.component.scss'
+  styleUrl: './local-admin-isms-suppliers.component.scss',
 })
-export class LocalAdminIsmsSuppliersComponent {
+export class LocalAdminIsmsSuppliersComponent extends BaseComponent {
+  constructor(private store: Store, private dialog: MatDialog) {
+    super();
+    this.store.dispatch(OrganizationActions.getOrganizationPermissions());
+  }
 
-
-  constructor(private store: Store) {
-    }
-  
-    public suppliers = [
+  public suppliers = [
     { Name: 'Supplier A', Cvr: '12345678', Uuid: '1' },
     { Name: 'Supplier B', Cvr: '87654321', Uuid: '2' },
     { Name: 'Supplier C', Cvr: '11223344', Uuid: '3' },
-  ]
+  ];
 
   public gridColumns = [
     { title: 'Navn', field: 'Name', hidden: false },
     { title: 'CVR', field: 'Cvr', hidden: false },
-    createGridActionColumn(['delete'])
+    createGridActionColumn(['delete']),
   ];
 
   public canModifyOrganization$ = this.store.select(selectOrganizationHasModifyPermission);
 
-  public removeSupplier($event: any){
-    //this.store.dispatch(OrganizationActions.removeSupplier({ Uuid: $event.Uuid }))
-    console.log($event)
+  public removeSupplier($event: any) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent);
+    const dialogInstance = dialogRef.componentInstance;
+    dialogInstance.bodyText = $localize`Er du sikker på, at du vil fjerne leverandøren "${$event.Name}"?`;
+    dialogInstance.confirmColor = 'warn';
+
+    this.subscriptions.add();
+    dialogRef
+      .afterClosed()
+      .pipe(first())
+      .subscribe((remove) => {
+        if (remove) {
+          //this.store.dispatch(OrganizationActions.removeSupplier({ Uuid: $event.Uuid }))
+          console.log('Removed supplier with Uuid: ' + $event.Uuid);
+        }
+      });
   }
 }
