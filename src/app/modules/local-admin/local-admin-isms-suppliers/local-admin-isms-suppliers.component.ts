481d975069ee6753af794f6831376517d809c2a0
NikolajNielsen <nan@strongminds.dk>
2025-09-23 13:53:44 +0200

    Scaffold add supplier dialog

--- src/app/modules/local-admin/local-admin-isms-suppliers/local-admin-isms-suppliers.component.ts
+++ src/app/modules/local-admin/local-admin-isms-suppliers/local-admin-isms-suppliers.component.ts
@@ -2,18 +2,21 @@ import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { first } from 'rxjs';
+import { first, of } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
+import { ButtonComponent } from 'src/app/shared/components/buttons/button/button.component';
 import { CardComponent } from 'src/app/shared/components/card/card.component';
 import { ConfirmationDialogComponent } from 'src/app/shared/components/dialogs/confirmation-dialog/confirmation-dialog.component';
+import { DropdownDialogComponent } from 'src/app/shared/components/dialogs/dropdown-dialog/dropdown-dialog.component';
 import { LocalGridComponent } from 'src/app/shared/components/local-grid/local-grid.component';
+import { OverviewHeaderComponent } from 'src/app/shared/components/overview-header/overview-header.component';
 import { createGridActionColumn } from 'src/app/shared/models/grid-action-column.model';
 import { OrganizationActions } from 'src/app/store/organization/actions';
 import { selectOrganizationHasModifyPermission } from 'src/app/store/organization/selectors';
 
 @Component({
   selector: 'app-local-admin-isms-suppliers',
-  imports: [CardComponent, LocalGridComponent, NgFor, AsyncPipe],
+  imports: [CardComponent, LocalGridComponent, NgFor, AsyncPipe, OverviewHeaderComponent, ButtonComponent],
   templateUrl: './local-admin-isms-suppliers.component.html',
   styleUrl: './local-admin-isms-suppliers.component.scss',
 })
@@ -29,13 +32,32 @@ export class LocalAdminIsmsSuppliersComponent extends BaseComponent {
     { Name: 'Supplier C', Cvr: '11223344', Uuid: '3' },
   ];
 
+  public suppliers$ = of(this.suppliers);
+
   public gridColumns = [
-    { title: 'Navn', field: 'Name', hidden: false },
+    { title: 'Virksomhed', field: 'Name', hidden: false },
     { title: 'CVR', field: 'Cvr', hidden: false },
     createGridActionColumn(['delete']),
   ];
 
   public canModifyOrganization$ = this.store.select(selectOrganizationHasModifyPermission);
+
+  public openAddSupplierDialog(){
+    const dialogRef = this.dialog.open(DropdownDialogComponent);
+    const dialogInstance = dialogRef.componentInstance;
+    dialogInstance.title = $localize`Tilføj leverandør`;
+    dialogInstance.data$ = this.suppliers$;
+    dialogInstance.valueField = 'Uuid';
+    dialogInstance.save.subscribe(($event: any) => {
+      this.saveSupplier($event);
+    })
+  }
+
+  public saveSupplier($event: any){
+    //todo this.store.dispatch(OrganizationActions.addSupplier({ Uuid: $event.Uuid }))
+    console.log('Added supplier with Uuid: ' + $event.Uuid);
+    this.dialog.closeAll();
+  }
 
   public removeSupplier($event: any) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent);
@@ -49,7 +71,7 @@ export class LocalAdminIsmsSuppliersComponent extends BaseComponent {
       .pipe(first())
       .subscribe((remove) => {
         if (remove) {
-          //this.store.dispatch(OrganizationActions.removeSupplier({ Uuid: $event.Uuid }))
+          //todo this.store.dispatch(OrganizationActions.removeSupplier({ Uuid: $event.Uuid }))
           console.log('Removed supplier with Uuid: ' + $event.Uuid);
         }
       });
