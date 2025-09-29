07c3e7a7f7a6dcce10ca90d1b2b5c5d864168fef
NikolajNielsen <nan@strongminds.dk>
2025-09-29 13:23:31 +0200

    Add no-op link control to edit dialog

--- src/app/modules/data-processing/data-processing-details/data-processing-oversight/oversights-table/write-oversight-date-dialog/write-oversight-date-dialog.component.ts
+++ src/app/modules/data-processing/data-processing-details/data-processing-oversight/oversights-table/write-oversight-date-dialog/write-oversight-date-dialog.component.ts
@@ -1,20 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { distinctUntilChanged, first } from 'rxjs';
+import { distinctUntilChanged, first, of } from 'rxjs';
 import { APIOversightDateDTO } from 'src/app/api/v2';
+import { EditUrlSectionComponent } from 'src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-gdpr/edit-url-section/edit-url-section.component';
 import { BaseComponent } from 'src/app/shared/base/base.component';
 import { optionalNewDate } from 'src/app/shared/helpers/date.helpers';
 import { DataProcessingActions } from 'src/app/store/data-processing/actions';
 import { selectDataProcessingOversightDates } from 'src/app/store/data-processing/selectors';
+import { ButtonComponent } from '../../../../../../shared/components/buttons/button/button.component';
+import { DatePickerComponent } from '../../../../../../shared/components/datepicker/datepicker.component';
+import { DialogActionsComponent } from '../../../../../../shared/components/dialogs/dialog-actions/dialog-actions.component';
 import { DialogComponent } from '../../../../../../shared/components/dialogs/dialog/dialog.component';
 import { StandardVerticalContentGridComponent } from '../../../../../../shared/components/standard-vertical-content-grid/standard-vertical-content-grid.component';
-import { DatePickerComponent } from '../../../../../../shared/components/datepicker/datepicker.component';
 import { TextAreaComponent } from '../../../../../../shared/components/textarea/textarea.component';
-import { DialogActionsComponent } from '../../../../../../shared/components/dialogs/dialog-actions/dialog-actions.component';
-import { ButtonComponent } from '../../../../../../shared/components/buttons/button/button.component';
 
 @Component({
   selector: 'app-write-oversight-date-dialog',
@@ -29,20 +30,28 @@ import { ButtonComponent } from '../../../../../../shared/components/buttons/but
     TextAreaComponent,
     DialogActionsComponent,
     ButtonComponent,
+    EditUrlSectionComponent,
   ],
 })
 export class WriteOversightDateDialogComponent extends BaseComponent implements OnInit {
   @Input() public oversightDate: APIOversightDateDTO | undefined;
 
+  public test$ = of({
+    url: 'https://www.kitos.dk',
+    name: 'someTitle',
+  });
+
   public oversightDateFormGroup = new FormGroup({
     date: new FormControl<Date | undefined>(undefined, Validators.required),
     notes: new FormControl<string | undefined>({ value: undefined, disabled: true }),
+    reportLinkUrl: new FormControl<string | undefined>(undefined),
+    reportLinkName: new FormControl<string | undefined>(undefined),
   });
 
   constructor(
     private store: Store,
     private dialogRef: MatDialogRef<WriteOversightDateDialogComponent>,
-    private actions$: Actions,
+    private actions$: Actions
   ) {
     super();
   }
@@ -59,6 +68,8 @@ export class WriteOversightDateDialogComponent extends BaseComponent implements 
       this.oversightDateFormGroup.patchValue({
         date: optionalNewDate(this.oversightDate.completedAt),
         notes: this.oversightDate.remark,
+        reportLinkUrl: this.oversightDate.oversightReportLink?.url,
+        reportLinkName: this.oversightDate.oversightReportLink?.name,
       });
       this.oversightDateFormGroup.controls.notes.enable();
     }
@@ -66,13 +77,13 @@ export class WriteOversightDateDialogComponent extends BaseComponent implements 
     this.subscriptions.add(
       this.actions$.pipe(ofType(DataProcessingActions.patchDataProcessingSuccess)).subscribe(() => {
         this.onCancel();
-      }),
+      })
     );
 
     this.subscriptions.add(
       this.actions$.pipe(ofType(DataProcessingActions.patchDataProcessingError)).subscribe(() => {
         this.isBusy = false;
-      }),
+      })
     );
 
     this.subscriptions.add(
@@ -82,7 +93,7 @@ export class WriteOversightDateDialogComponent extends BaseComponent implements 
         } else {
           this.oversightDateFormGroup.controls.notes.disable();
         }
-      }),
+      })
     );
   }
 
@@ -106,8 +117,8 @@ export class WriteOversightDateDialogComponent extends BaseComponent implements 
           this.store.dispatch(
             DataProcessingActions.patchDataProcessingOversightDate(
               { ...request, uuid: this.oversightDate?.uuid },
-              oversightDates,
-            ),
+              oversightDates
+            )
           );
         } else {
           this.store.dispatch(DataProcessingActions.addDataProcessingOversightDate(request, oversightDates));
