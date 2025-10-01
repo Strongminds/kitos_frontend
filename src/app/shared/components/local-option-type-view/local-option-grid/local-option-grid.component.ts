37f51660d43f7268788b9cc82b14b2fa421d22b7
Aleksander Naskret <101557040+strongmindsaln@users.noreply.github.com>
2025-10-01 12:40:13 +0200

    Feature/kitosudv 6020 - externally used option (#541)
    * update swagger
    
    * show external use flag
    
    * grid options
    
    * support external use edit
    
    * update messages
    
    * update-swagger
    
    * shortcircuit edit component
    
    * swagger
    
    * swagger
    
    * show in overviews
    
    * translations
    
    * cleanuop
    
    * cleanup 2

--- src/app/shared/components/local-option-type-view/local-option-grid/local-option-grid.component.ts
+++ src/app/shared/components/local-option-type-view/local-option-grid/local-option-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { OBLIGATORY_LOCAL_OPTION_HELP_TEXT } from 'src/app/shared/constants/constants';
 import { createGridActionColumn } from 'src/app/shared/models/grid-action-column.model';
 import { GridColumn } from 'src/app/shared/models/grid-column.model';
 import { BooleanChange } from 'src/app/shared/models/grid/grid-events.model';
@@ -9,9 +10,8 @@ import {
   LocalAdminOptionTypeItem,
 } from 'src/app/shared/models/options/local-admin-option-type.model';
 import { LocalOptionTypeActions } from 'src/app/store/local-admin/local-option-types/actions';
-import { EditLocalOptionTypeDialogComponent } from '../edit-local-option-type-dialog/edit-local-option-type-dialog.component';
-import { OBLIGATORY_LOCAL_OPTION_HELP_TEXT } from 'src/app/shared/constants/constants';
 import { LocalGridComponent } from '../../local-grid/local-grid.component';
+import { EditLocalOptionTypeDialogComponent } from '../edit-local-option-type-dialog/edit-local-option-type-dialog.component';
 
 @Component({
   selector: 'app-local-option-grid',
@@ -27,8 +27,10 @@ export class LocalOptionGridComponent implements OnInit {
   @Input() fitSizeToContent = true;
 
   @Input() showWriteAccess: boolean = false;
+  @Input() showExternalUse: boolean = false;
   @Input() showDescription: boolean = true;
   @Input() showEditButton: boolean = true;
+
   private readonly gridColumns: GridColumn[] = [
     {
       field: 'active',
@@ -58,15 +60,24 @@ export class LocalOptionGridComponent implements OnInit {
       title: $localize`Beskrivelse`,
       hidden: false,
     },
+    {
+      field: 'isExternallyUsed',
+      title: $localize`Bruges eksternt`,
+      hidden: false,
+      style: 'boolean',
+      noFilter: true,
+    },
+    {
+      field: 'externallyUsedDescription',
+      title: $localize`Beskrivelse af ekstern brug`,
+      hidden: false,
+    },
     createGridActionColumn(['edit']),
   ];
 
   public filteredGridColumns!: GridColumn[];
 
-  constructor(
-    private dialog: MatDialog,
-    private store: Store,
-  ) {}
+  constructor(private dialog: MatDialog, private store: Store) {}
 
   public ngOnInit(): void {
     this.filteredGridColumns = this.gridColumns.map((column) => {
@@ -75,6 +86,10 @@ export class LocalOptionGridComponent implements OnInit {
           return { ...column, hidden: !this.showWriteAccess };
         case 'description':
           return { ...column, hidden: !this.showDescription };
+        case 'isExternallyUsed':
+          return { ...column, hidden: !this.showExternalUse };
+        case 'externallyUsedDescription':
+          return { ...column, hidden: !this.showExternalUse };
         case 'Actions':
           return { ...column, hidden: !this.showEditButton };
         default:
@@ -93,7 +108,7 @@ export class LocalOptionGridComponent implements OnInit {
     const activeStatus = event.value;
     const option = event.item;
     this.store.dispatch(
-      LocalOptionTypeActions.updateOptionTypeActiveStatus(this.optionType, option.uuid, activeStatus),
+      LocalOptionTypeActions.updateOptionTypeActiveStatus(this.optionType, option.uuid, activeStatus)
     );
   }
 }
