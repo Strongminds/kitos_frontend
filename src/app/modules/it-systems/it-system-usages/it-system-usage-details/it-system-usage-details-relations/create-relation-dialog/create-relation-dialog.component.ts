a0c5c22c569cfe6f317cbe8a96ee415de8c3d4c1
NikolajNielsen <nan@strongminds.dk>
2025-10-21 09:17:10 +0200

    Pull apart create and modify dialogs to allow for separate interface count

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-relations/create-relation-dialog/create-relation-dialog.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-relations/create-relation-dialog/create-relation-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { APIIdentityNamePairResponseDTO, APISystemRelationWriteRequestDTO } from 'src/app/api/v2';
@@ -6,9 +6,24 @@ import { BaseComponent } from 'src/app/shared/base/base.component';
 import { ITSystemUsageActions } from 'src/app/store/it-system-usage/actions';
 import { ItSystemUsageDetailsRelationsDialogComponentStore } from '../system-relation-dialog/relation-dialog.component-store';
 import {
-  SystemRelationDialogFormModel,
   SystemRelationDialogComponent,
 } from '../system-relation-dialog/system-relation-dialog.component';
+import { combineLatest, first, map, Subject } from 'rxjs';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Actions, ofType } from '@ngrx/effects';
+import { ModifyRelationDialogComponent } from '../modify-relation-dialog/modify-relation-dialog.component';
+import { selectRegularOptionTypes } from 'src/app/store/regular-option-type-store/selectors';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
+
+export interface SystemRelationCreateDialogFormModel {
+  systemUsage: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+  description: FormControl<string | null | undefined>;
+  reference: FormControl<string | null | undefined>;
+  contract: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+  interfaces: FormControl<APIIdentityNamePairResponseDTO[] | null | undefined>;
+  frequency: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+}
 
 @Component({
   selector: 'app-create-relation-dialog',
@@ -18,7 +33,7 @@ import {
   imports: [SystemRelationDialogComponent],
 })
 export class CreateRelationDialogComponent extends BaseComponent {
-  public relationForm = new FormGroup<SystemRelationDialogFormModel>({
+  public relationForm = new FormGroup<SystemRelationCreateDialogFormModel>({
     systemUsage: new FormControl<APIIdentityNamePairResponseDTO | undefined>(
       { value: undefined, disabled: false },
       Validators.required,
@@ -26,15 +41,157 @@ export class CreateRelationDialogComponent extends BaseComponent {
     description: new FormControl<string | undefined>({ value: undefined, disabled: true }),
     reference: new FormControl<string | undefined>({ value: undefined, disabled: true }),
     contract: new FormControl<APIIdentityNamePairResponseDTO | undefined>({ value: undefined, disabled: true }),
-    interface: new FormControl<APIIdentityNamePairResponseDTO | undefined>({ value: undefined, disabled: true }),
+    interfaces: new FormControl<APIIdentityNamePairResponseDTO[] | undefined>({ value: undefined, disabled: true }),
     frequency: new FormControl<APIIdentityNamePairResponseDTO | undefined>({ value: undefined, disabled: true }),
   });
 
-  constructor(private readonly store: Store) {
-    super();
-  }
+    @Input() public title!: string;
+    @Input() public saveText!: string;
+    @Output() public saveRequested = new EventEmitter<APISystemRelationWriteRequestDTO>();
 
-  public onSave(request: APISystemRelationWriteRequestDTO) {
-    this.store.dispatch(ITSystemUsageActions.addItSystemUsageRelation(request));
-  }
+    public readonly systemUsages$ = this.componentStore.systemUsages$;
+    public readonly systemUsagesLoading$ = this.componentStore.isSystemUsagesLoading$;
+    public readonly contracts$ = this.componentStore.contracts$;
+    public readonly contractsLoading$ = this.componentStore.contractsLoading$;
+    public readonly interfaces$ = this.componentStore.interfaces$;
+
+    public readonly interfacesLoading$ = this.componentStore.isInterfacesOrSystemUuidLoading$;
+
+    public readonly usageSearchResultIsLimited$ = this.componentStore.usageSearchResultIsLimited$;
+
+    public readonly availableReferenceFrequencyTypes$ = this.store
+      .select(selectRegularOptionTypes('it-system_usage-relation-frequency-type'))
+      .pipe(filterNullish());
+
+    //current system Uuid (system, not system usage)
+    private readonly selectedSystemUuid$ = this.componentStore.systemUuid$;
+
+    //selected usage uuids
+    private readonly changedSystemUsageUuid$ = this.componentStore.changedSystemUsageUuid$;
+    //interface search terms
+    private readonly searchInterfaceTerm$ = new Subject<string | undefined>();
+
+    public isBusy = false;
+
+    constructor(
+      protected readonly store: Store,
+      protected readonly componentStore: ItSystemUsageDetailsRelationsDialogComponentStore,
+      private readonly dialog: MatDialogRef<ModifyRelationDialogComponent>,
+      private readonly actions$: Actions
+    ) {
+      super();
+    }
+
+    ngOnInit(): void {
+      this.store.dispatch(RegularOptionTypeActions.getOptions('it-system_usage-relation-frequency-type'));
+
+      //on selected system usage change or interface search change, load the interfaces
+      this.subscriptions.add(
+        combineLatest([this.selectedSystemUuid$, this.searchInterfaceTerm$])
+          .pipe(map(([systemUuid, searchTerm]) => ({ systemUuid, searchTerm })))
+          .subscribe(({ systemUuid, searchTerm }) => {
+            console.log(`Searching interfaces for system ${systemUuid} with term "${searchTerm}"`);
+            this.componentStore.getItInterfaces({ systemUuid: systemUuid, search: searchTerm });
+          })
+      );
+
+      //when usage is selected enable the form, otherwise turn it off (other than the usage dropdown)
+      this.subscriptions.add(
+        this.changedSystemUsageUuid$.subscribe((usageUuid) => {
+          this.relationForm.controls.interfaces.reset();
+          if (usageUuid) {
+            this.relationForm.enable();
+          } else {
+            this.relationForm.disable();
+            this.relationForm.controls['systemUsage'].enable();
+          }
+        })
+      );
+
+      //on success close the dialog
+      this.subscriptions.add(
+        this.actions$
+          .pipe(
+            ofType(
+              ITSystemUsageActions.addItSystemUsageRelationSuccess,
+              ITSystemUsageActions.patchItSystemUsageRelationSuccess
+            ),
+            first()
+          )
+          .subscribe(() => this.dialog.close())
+      );
+
+      //on error set isBusy to false
+      this.subscriptions.add(
+        this.actions$
+          .pipe(
+            ofType(
+              ITSystemUsageActions.addItSystemUsageRelationError,
+              ITSystemUsageActions.patchItSystemUsageRelationError
+            )
+          )
+          .subscribe(() => {
+            this.isBusy = false;
+          })
+      );
+    }
+
+    public usageFilterChange(search?: string) {
+      this.componentStore.getItSystemUsages(search);
+    }
+
+    public contractFilterChange(search?: string) {
+      this.componentStore.getItContracts(search);
+    }
+
+    public interfaceFilterChange(search?: string) {
+      this.searchInterfaceTerm$.next(search);
+    }
+
+    public interfaceValueChange(newInterfaces: APIIdentityNamePairResponseDTO[]) {
+      console.log('Selected interface: ', JSON.stringify(newInterfaces));
+      this.relationForm.controls.interfaces.setValue(newInterfaces);
+    }
+
+    public usageChange(usageUuid?: string) {
+      this.componentStore.updateCurrentSystemUuid(usageUuid);
+    }
+
+    public save() {
+      if (!this.relationForm.valid) return;
+
+      const usage = this.relationForm.value.systemUsage;
+      if (!usage) return;
+
+      this.isBusy = true;
+
+      const formValue = this.relationForm.value;
+      const requests = formValue.interfaces === undefined || formValue.interfaces === null || formValue.interfaces.length === 0
+        ? [
+          {
+        toSystemUsageUuid: usage.uuid,
+        relationInterfaceUuid: undefined,
+        associatedContractUuid: this.relationForm.value.contract?.uuid,
+        relationFrequencyUuid: this.relationForm.value.frequency?.uuid,
+        description: this.relationForm.value.description ?? undefined,
+        urlReference: this.relationForm.value.reference ?? undefined,
+      }
+        ]
+        : formValue.interfaces.map((x) => (
+          {
+            toSystemUsageUuid: usage.uuid,
+            relationInterfaceUuid: x.uuid,
+            associatedContractUuid: this.relationForm.value.contract?.uuid,
+            relationFrequencyUuid: this.relationForm.value.frequency?.uuid,
+            description: this.relationForm.value.description ?? undefined,
+            urlReference: this.relationForm.value.reference ?? undefined,
+        })
+      );
+//TODO implement action for multi-create
+//      this.store.dispatch(ITSystemUsageActions.addItSystemUsageRelation(requests));
+    }
+
+    public close() {
+      this.dialog.close();
+    }
 }
--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-relations/modify-relation-dialog/modify-relation-dialog.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-relations/modify-relation-dialog/modify-relation-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { APIIdentityNamePairResponseDTO, APISystemRelationWriteRequestDTO } from 'src/app/api/v2';
@@ -7,9 +7,24 @@ import { ITSystemUsageActions } from 'src/app/store/it-system-usage/actions';
 import { SystemRelationModel } from '../relation-table/relation-table.component';
 import { ItSystemUsageDetailsRelationsDialogComponentStore } from '../system-relation-dialog/relation-dialog.component-store';
 import {
-  SystemRelationDialogFormModel,
   SystemRelationDialogComponent,
 } from '../system-relation-dialog/system-relation-dialog.component';
+import { selectRegularOptionTypes } from 'src/app/store/regular-option-type-store/selectors';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { combineLatest, first, map, Subject } from 'rxjs';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Actions, ofType } from '@ngrx/effects';
+import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
+import { IdentityNamePair } from 'src/app/shared/models/identity-name-pair.model';
+
+export interface SystemRelationModifyDialogFormModel {
+  systemUsage: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+  description: FormControl<string | null | undefined>;
+  reference: FormControl<string | null | undefined>;
+  contract: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+  interface: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+  frequency: FormControl<APIIdentityNamePairResponseDTO | null | undefined>;
+}
 
 @Component({
   selector: 'app-modify-relation-dialog[relationModel]',
@@ -23,15 +38,97 @@ export class ModifyRelationDialogComponent extends BaseComponent implements OnIn
 
   public relationForm!: FormGroup<SystemRelationDialogFormModel>;
 
+    @Input() public title!: string;
+  @Input() public saveText!: string;
+  @Output() public saveRequested = new EventEmitter<APISystemRelationWriteRequestDTO>();
+
+  public readonly systemUsages$ = this.componentStore.systemUsages$;
+  public readonly systemUsagesLoading$ = this.componentStore.isSystemUsagesLoading$;
+  public readonly contracts$ = this.componentStore.contracts$;
+  public readonly contractsLoading$ = this.componentStore.contractsLoading$;
+  public readonly interfaces$ = this.componentStore.interfaces$;
+
+  public readonly interfacesLoading$ = this.componentStore.isInterfacesOrSystemUuidLoading$;
+
+  public readonly usageSearchResultIsLimited$ = this.componentStore.usageSearchResultIsLimited$;
+
+  public readonly availableReferenceFrequencyTypes$ = this.store
+    .select(selectRegularOptionTypes('it-system_usage-relation-frequency-type'))
+    .pipe(filterNullish());
+
+  //current system Uuid (system, not system usage)
+  private readonly selectedSystemUuid$ = this.componentStore.systemUuid$;
+
+  //selected usage uuids
+  private readonly changedSystemUsageUuid$ = this.componentStore.changedSystemUsageUuid$;
+  //interface search terms
+  private readonly searchInterfaceTerm$ = new Subject<string | undefined>();
+
+  public isBusy = false;
+
   constructor(
-    private readonly store: Store,
-    private readonly componentStore: ItSystemUsageDetailsRelationsDialogComponentStore,
+    protected readonly store: Store,
+    protected readonly componentStore: ItSystemUsageDetailsRelationsDialogComponentStore,
+    private readonly dialog: MatDialogRef<ModifyRelationDialogComponent>,
+    private readonly actions$: Actions
   ) {
     super();
   }
 
-  public ngOnInit(): void {
-    this.relationForm = new FormGroup<SystemRelationDialogFormModel>({
+  ngOnInit(): void {
+    this.store.dispatch(RegularOptionTypeActions.getOptions('it-system_usage-relation-frequency-type'));
+
+    //on selected system usage change or interface search change, load the interfaces
+    this.subscriptions.add(
+      combineLatest([this.selectedSystemUuid$, this.searchInterfaceTerm$])
+        .pipe(map(([systemUuid, searchTerm]) => ({ systemUuid, searchTerm })))
+        .subscribe(({ systemUuid, searchTerm }) => {
+          console.log(`Searching interfaces for system ${systemUuid} with term "${searchTerm}"`);
+          this.componentStore.getItInterfaces({ systemUuid: systemUuid, search: searchTerm });
+        })
+    );
+
+    //when usage is selected enable the form, otherwise turn it off (other than the usage dropdown)
+    this.subscriptions.add(
+      this.changedSystemUsageUuid$.subscribe((usageUuid) => {
+        this.relationForm.controls.interface.reset();
+        if (usageUuid) {
+          this.relationForm.enable();
+        } else {
+          this.relationForm.disable();
+          this.relationForm.controls['systemUsage'].enable();
+        }
+      })
+    );
+
+    //on success close the dialog
+    this.subscriptions.add(
+      this.actions$
+        .pipe(
+          ofType(
+            ITSystemUsageActions.addItSystemUsageRelationSuccess,
+            ITSystemUsageActions.patchItSystemUsageRelationSuccess
+          ),
+          first()
+        )
+        .subscribe(() => this.dialog.close())
+    );
+
+    //on error set isBusy to false
+    this.subscriptions.add(
+      this.actions$
+        .pipe(
+          ofType(
+            ITSystemUsageActions.addItSystemUsageRelationError,
+            ITSystemUsageActions.patchItSystemUsageRelationError
+          )
+        )
+        .subscribe(() => {
+          this.isBusy = false;
+        })
+    );
+
+    this.relationForm = new FormGroup<SystemRelationModifyDialogFormModel>({
       systemUsage: new FormControl<APIIdentityNamePairResponseDTO | undefined>(
         {
           value: this.relationModel.systemUsage,
@@ -59,7 +156,56 @@ export class ModifyRelationDialogComponent extends BaseComponent implements OnIn
     this.componentStore.updateCurrentSystemUuid(this.relationModel.systemUsage.uuid);
   }
 
-  public onModifySave(request: APISystemRelationWriteRequestDTO) {
+  public usageFilterChange(search?: string) {
+    this.componentStore.getItSystemUsages(search);
+  }
+
+  public contractFilterChange(search?: string) {
+    this.componentStore.getItContracts(search);
+  }
+
+  public interfaceFilterChange(search?: string) {
+    this.searchInterfaceTerm$.next(search);
+  }
+
+  public interfaceValueChange(newInterface: IdentityNamePair) {
+    console.log('Selected interface: ', JSON.stringify(newInterface));
+    this.relationForm.controls.interface.setValue(newInterface);
+    // this.relationForm.controls.interface.setValue(
+    //   interfaceUuid
+    //     ? {
+    //         uuid: interfaceUuid,
+    //         name: '',
+    //       }
+    //     : null
+    // );
+  }
+
+  public usageChange(usageUuid?: string) {
+    this.componentStore.updateCurrentSystemUuid(usageUuid);
+  }
+
+  public save() {
+    if (!this.relationForm.valid) return;
+
+    const usage = this.relationForm.value.systemUsage;
+    if (!usage) return;
+
+    this.isBusy = true;
+
+    const request = {
+      toSystemUsageUuid: usage.uuid,
+      relationInterfaceUuid: this.relationForm.value.interface?.uuid,
+      associatedContractUuid: this.relationForm.value.contract?.uuid,
+      relationFrequencyUuid: this.relationForm.value.frequency?.uuid,
+      description: this.relationForm.value.description ?? undefined,
+      urlReference: this.relationForm.value.reference ?? undefined,
+    };
+
     this.store.dispatch(ITSystemUsageActions.patchItSystemUsageRelation(this.relationModel.uuid, request));
+  }
+
+  public close() {
+    this.dialog.close();
   }
 }
