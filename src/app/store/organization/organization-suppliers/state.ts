c1ccf40f52d5766e07191ba1fca68edb4f0a863a
Aleksander Naskret <aln@strongminds.dk>
2025-10-23 13:57:26 +0200

    fix build issues

--- src/app/store/organization/organization-suppliers/state.ts
+++ src/app/store/organization/organization-suppliers/state.ts
@@ -1,6 +1,7 @@
-import { ShallowOrganization } from "src/app/shared/models/organization/shallow-organization.model";
+import { EntityState } from '@ngrx/entity';
+import { ShallowOrganization } from 'src/app/shared/models/organization/shallow-organization.model';
 
-export interface OrganizationSuppliersState {
+export interface OrganizationSuppliersState extends EntityState<ShallowOrganization> {
   suppliers: ShallowOrganization[];
   suppliersCacheTime: number | undefined;
   availableSuppliers: ShallowOrganization[];
