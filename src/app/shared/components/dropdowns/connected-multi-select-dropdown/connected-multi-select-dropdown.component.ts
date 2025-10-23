348b157a8ad2e93e5732410ee7a7ff55a6923be5
NikolajNielsen <nan@strongminds.dk>
2025-10-23 07:44:14 +0200

    Add connected dropdown base class

--- src/app/shared/components/dropdowns/connected-multi-select-dropdown/connected-multi-select-dropdown.component.ts
+++ src/app/shared/components/dropdowns/connected-multi-select-dropdown/connected-multi-select-dropdown.component.ts
@@ -5,6 +5,7 @@ import { map, Observable, pairwise, startWith, Subject } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
 import { MultiSelectDropdownItem } from 'src/app/shared/models/dropdown-option.model';
 import { MultiSelectDropdownComponent } from '../multi-select-dropdown/multi-select-dropdown.component';
+import { ConnectedDropdownBaseComponent } from '../connected-dropdown-base/connected-dropdown-base.component';
 
 @Component({
   selector: 'app-connected-multi-select-dropdown',
@@ -12,65 +13,12 @@ import { MultiSelectDropdownComponent } from '../multi-select-dropdown/multi-sel
   templateUrl: './connected-multi-select-dropdown.component.html',
   styleUrl: './connected-multi-select-dropdown.component.scss',
 })
-export class ConnectedMultiSelectDropdownComponent<T> extends BaseComponent {
-  @Input() public text!: string;
-  @Input() public textField: string = 'name';
-  @Input() public itemDescriptionField: string = 'description';
-  @Input() public valueField!: string;
+export class ConnectedMultiSelectDropdownComponent<T> extends ConnectedDropdownBaseComponent {
   @Input() public data$?: Observable<MultiSelectDropdownItem<T>[]>;
-  @Input() public isLoading$?: Observable<boolean>;
-  @Input() public showSearchHelpText$?: Observable<boolean>;
-  @Input() public includeItemDescription = false;
-  @Input() public addTag = false;
-  @Input() public addTagText = $localize`Vælg`;
   @Input() public resetSubject$?: Subject<void>;
-  @Output() public filterChange = new EventEmitter<string>();
   @Output() public valueChange = new EventEmitter<T[]>();
 
-  private searchTermsSource$ = new Subject<string | undefined>();
-  private lastTwoSearchTerms$ = this.searchTermsSource$.pipe(
-    startWith(undefined),
-    pairwise(),
-    map(([previous, current]) => ({ previous: previous, current: current }))
-  );
-
-  ngOnInit() {
-    this.filterChange.emit(undefined);
-    this.subscriptions.add(
-      this.lastTwoSearchTerms$.subscribe((terms) => {
-        if (terms.previous != terms.current) {
-          this.filterChange.emit(terms.current);
-        }
-      })
-    );
-  }
-
-  //since the dropdown is filtered externally, accept every item
-  public externalSearch(_: string, __: any) {
-    return true;
-  }
-
-  public onValueChange(value?: any) {
+  public onValueChange(value?: T[]) {
     this.valueChange.emit(value);
-  }
-
-  public onFilterChange(searchTerm?: string) {
-    this.publishActiveSearchTerm(searchTerm);
-  }
-
-  public onFocus() {
-    this.resetActiveSearchTerm();
-  }
-
-  private publishActiveSearchTerm(term?: string) {
-    this.searchTermsSource$.next(term);
-  }
-
-  public onOpen() {
-    this.resetActiveSearchTerm();
-  }
-
-  private resetActiveSearchTerm() {
-    this.publishActiveSearchTerm();
   }
 }
