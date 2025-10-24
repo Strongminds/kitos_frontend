0548b2fa9c1b8066a6efa6517c8282b251d8cad8
NikolajNielsen <nan@strongminds.dk>
2025-10-24 10:09:43 +0200

    api

--- src/app/api/v2/api/v2DataProcessingRegistrationOversightDates.service.ts
+++ src/app/api/v2/api/v2DataProcessingRegistrationOversightDates.service.ts
@@ -57,7 +57,7 @@ export interface PostSingleDataProcessingRegistrationOversightDatesV2PostDataPro
 })
 export class APIV2DataProcessingRegistrationOversightDatesService {
 
-    protected basePath = 'https://kitos-dev.strongminds.dk';
+    protected basePath = 'https://localhost:44300';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
