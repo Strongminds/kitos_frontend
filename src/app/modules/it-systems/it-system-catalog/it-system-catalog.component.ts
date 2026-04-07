de15b7322e286e5b1376f368af370b4bdacc7805
NikolajNielsen <nan@strongminds.dk>
2026-04-07 10:12:29 +0200

    Remove duplicate  identifier by defaulting to field if no datafield

--- src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
+++ src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
@@ -146,7 +146,6 @@ export class ItSystemCatalogComponent extends BaseOverviewComponent implements O
     },
     {
       field: CatalogFields.BUSINESS_TYPE_NAME,
-      dataField: CatalogFields.BUSINESS_TYPE_NAME,
       title: $localize`Forretningstype`,
       section: this.systemSectionName,
       extraData: 'it-system_business-type',
