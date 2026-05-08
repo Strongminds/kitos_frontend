09681d7f823f6a0849a9ddd7d893f83ffbdaef32
Aleksander Naskret <aln@strongminds.dk>
2026-05-08 08:31:10 +0200

    swagger update

--- src/app/api/v2/model/gDPRWriteRequestDTO.ts
+++ src/app/api/v2/model/gDPRWriteRequestDTO.ts
@@ -19,7 +19,6 @@ import { APIRiskLevelChoice } from './riskLevelChoice';
 
 
 export interface APIGDPRWriteRequestDTO { 
-    purpose?: string | null;
     hostedAt?: APIHostingChoice;
     directoryDocumentation?: APISimpleLinkDTO;
     dataSensitivityLevels?: Array<APIDataSensitivityLevelChoice> | null;
