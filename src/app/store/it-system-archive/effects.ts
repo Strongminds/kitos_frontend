2c812ad040355d4bd95fba7b9d35497e8ad14c99
Aleksander Naskret <aln@strongminds.dk>
2026-06-29 09:10:02 +0200

    add collection permissions call

--- src/app/store/it-system-archive/effects.ts
+++ src/app/store/it-system-archive/effects.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { concatLatestFrom } from '@ngrx/operators';
 import { Store } from '@ngrx/store';
 import { compact } from 'lodash';
 import { catchError, map, of, switchMap } from 'rxjs';
-import { ItSystemArchiveV2Service } from 'src/app/api/v2';
+import { ItSystemUsageArchiveV2Service } from 'src/app/api/v2';
 import { ARCHIVE_COLUMNS_ID } from 'src/app/shared/constants/persistent-state-constants';
 import { adaptItSystemArchive } from 'src/app/shared/models/it-system/it-system-archive-odata.model';
 import { OData } from 'src/app/shared/models/odata.model';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
 import { GridColumnStorageService } from 'src/app/shared/services/grid-column-storage-service';
 import { GridDataCacheService } from 'src/app/shared/services/grid-data-cache.service';
 import { selectOrganizationUuid } from '../user-store/selectors';
@@ -20,16 +21,19 @@ export class ITSystemArchiveEffects {
   constructor(
     private actions$: Actions,
     private store: Store,
-    private archiveService: ItSystemArchiveV2Service,
+    @Inject(ItSystemUsageArchiveV2Service) private archiveService: ItSystemUsageArchiveV2Service,
     private httpClient: HttpClient,
     private gridColumnStorageService: GridColumnStorageService,
     private gridDataCacheService: GridDataCacheService,
-  ) { }
+  ) {}
 
   getArchives$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ITSystemArchiveActions.getITSystemArchives),
-      concatLatestFrom(() => [this.store.select(selectArchivePreviousGridState), this.store.select(selectOrganizationUuid)]),
+      concatLatestFrom(() => [
+        this.store.select(selectArchivePreviousGridState),
+        this.store.select(selectOrganizationUuid),
+      ]),
       switchMap(([{ gridState }, previousGridState, organizationUuid]) => {
         this.gridDataCacheService.tryResetOnGridStateChange(gridState, previousGridState);
 
@@ -79,7 +83,7 @@ export class ITSystemArchiveEffects {
     return this.actions$.pipe(
       ofType(ITSystemArchiveActions.deleteITSystemArchive),
       switchMap(({ archiveUuid }) =>
-        this.archiveService.deleteSingleItSystemArchiveV2Delete({ archiveUuid }).pipe(
+        this.archiveService.deleteSingleItSystemUsageArchiveV2Delete({ archiveUuid }).pipe(
           map(() => {
             this.gridDataCacheService.reset();
             return ITSystemArchiveActions.deleteITSystemArchiveSuccess();
@@ -93,7 +97,15 @@ export class ITSystemArchiveEffects {
   getCollectionPermissions$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ITSystemArchiveActions.getITSystemArchiveCollectionPermissions),
-      map(() => ITSystemArchiveActions.getITSystemArchiveCollectionPermissionsSuccess(undefined)),
+      concatLatestFrom(() => this.store.select(selectOrganizationUuid).pipe(filterNullish())),
+      switchMap(([_, organizationUuid]) =>
+        this.archiveService
+          .getSingleItSystemUsageArchiveV2GetItSystemUsageArchiveCollectionPermissions({ organizationUuid })
+          .pipe(
+            map((permissions) => ITSystemArchiveActions.getITSystemArchiveCollectionPermissionsSuccess(permissions)),
+            catchError(() => of(ITSystemArchiveActions.getITSystemArchiveCollectionPermissionsError())),
+          ),
+      ),
     );
   });
 }
