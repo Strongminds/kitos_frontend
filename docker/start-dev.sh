e7e3ea70a557c02f773da883352579dcc8ab297a
Aleksander Naskret <aln@strongminds.dk>
2026-07-23 10:17:00 +0200

    ensure BACKEND_URL is read from env

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -22,4 +22,4 @@ cat > /tmp/proxy.conf.json <<EOF
 }
 EOF
 
-yarn ng serve --configuration=docker --port=4200 --poll=2000 --proxy-config=/tmp/proxy.conf.json
+yarn ng serve --port=4200 --poll=2000 --proxy-config=/tmp/proxy.conf.json
