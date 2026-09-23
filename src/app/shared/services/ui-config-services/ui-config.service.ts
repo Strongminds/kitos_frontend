a603c648a2aff554a79f20d8be3483e8eaaf8976
Aleksander Naskret <aln@strongminds.dk>
2026-09-23 09:25:29 +0200

    remove un-used method

--- src/app/shared/services/ui-config-services/ui-config.service.ts
+++ src/app/shared/services/ui-config-services/ui-config.service.ts
@@ -16,32 +16,11 @@ import { UINodeCustomization } from '../../models/ui-config/ui-node-customizatio
 export class UIConfigService {
   public buildUIModuleConfig(uiModuleCustomizations: UINodeCustomization[], module: UIModuleConfigKey): UIModuleConfig {
     const blueprint = this.getUIBlueprintWithFullKeys(module);
-    const normalizedCustomizations = this.normalizeLegacyRecommendations(uiModuleCustomizations, module);
     const moduleConfigViewModel: UIConfigNodeViewModel | undefined = this.buildUIConfigNodeViewModels(
       blueprint,
-      normalizedCustomizations,
+      uiModuleCustomizations,
     );
     return { module, moduleConfigViewModel, cacheTime: undefined };
-  }
-
-  private normalizeLegacyRecommendations(
-    customizations: UINodeCustomization[],
-    module: UIModuleConfigKey,
-  ): UINodeCustomization[] {
-    if (module !== UIModuleConfigKey.DataProcessingRegistrations) return customizations;
-
-    const mainContractKey = `${module}.itContracts.mainContract`;
-    const associatedContractsKey = `${module}.itContracts.associatedContracts`;
-    const mainContract = customizations.find((node) => node.fullKey === mainContractKey);
-    const associatedContracts = customizations.find((node) => node.fullKey === associatedContractsKey);
-
-    if (mainContract?.recommended !== undefined || associatedContracts?.recommended !== true) return customizations;
-
-    if (!mainContract) return [...customizations, { fullKey: mainContractKey, recommended: true }];
-
-    return customizations.map((node) =>
-      node.fullKey === mainContractKey ? { ...node, recommended: true } : node,
-    );
   }
 
   public getAllNodesOfBlueprint(moduleKey: UIModuleConfigKey): SimpleUINodeBlueprint[] {
